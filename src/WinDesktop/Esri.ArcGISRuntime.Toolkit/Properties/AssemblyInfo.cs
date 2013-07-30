900c2601b4c3df7bd7ffaa095d41315d1d3ad00a
Dominique Broux <dbroux@esri.com>
2013-07-30 09:59:18 +0200

    Added AssemblyDescription

--- src/WinDesktop/Esri.ArcGISRuntime.Toolkit/Properties/AssemblyInfo.cs
+++ src/WinDesktop/Esri.ArcGISRuntime.Toolkit/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Esri.ArcGISRuntime.Toolkit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ArcGIS Runtime Desktop Toolkit for Microsoft.Net")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Esri.ArcGISRuntime.Toolkit")]
