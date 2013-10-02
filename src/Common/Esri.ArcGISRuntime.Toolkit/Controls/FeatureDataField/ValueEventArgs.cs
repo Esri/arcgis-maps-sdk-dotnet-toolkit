39e2c058cfd4d0e3d956cb27a5038ebb8e386adc
chrisahill1 <chrisahill1@outlook.com>
2013-10-02 13:01:51 -0700

    Added FeatureDataField toolkit control for WinDesktop, WinPhone and WinStore

---
+++ src/Common/Esri.ArcGISRuntime.Toolkit/Controls/FeatureDataField/ValueEventArgs.cs
@@ -0,0 +1,24 @@
+﻿namespace Esri.ArcGISRuntime.Toolkit.Controls
+{
+    /// <summary>
+    /// Base class for ValueChangeing and ValueChange events for FeatureDataField.
+    /// </summary>
+    public abstract class ValueEventArgs
+    {
+        internal ValueEventArgs(object oldValue, object newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+
+        /// <summary>
+        /// Gets the old value.
+        /// </summary>
+        public object OldValue { get; private set; }
+
+        /// <summary>
+        /// Gets the new value.
+        /// </summary>
+        public object NewValue { get; private set; }
+    }
+}
