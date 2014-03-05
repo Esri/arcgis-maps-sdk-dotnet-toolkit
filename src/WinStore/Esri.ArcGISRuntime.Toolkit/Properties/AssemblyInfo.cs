f13edca130549bd3d7e44861a0a90aac04f939e2
Morten Nielsen <mnielsen@esri.com>
2014-03-04 20:15:08 -0800

    Added license headers

--- src/WinStore/Esri.ArcGISRuntime.Toolkit/Properties/AssemblyInfo.cs
+++ src/WinStore/Esri.ArcGISRuntime.Toolkit/Properties/AssemblyInfo.cs
@@ -1,4 +1,9 @@
-﻿using System.Reflection;
+﻿// (c) Copyright ESRI.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
