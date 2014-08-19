50af2a9d24166f81483660929b33ea7fb6cd9467
Antti Kajanus <antti.kajanus@live.com>
2014-08-19 17:39:56 +0100

    Added new samples to desktop

---
+++ samples/WinDesktop/ArcGISRuntime.Toolkit.Samples/ScaleLine/ScaleLineStylingSample.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArcGISRuntime.Toolkit.Samples.Desktop.ScaleLine
+{
+	/// <summary>
+	/// Demonstrates how to style ScaleLine control.
+	/// </summary>
+	/// <title>Styling ScaleLine</title>
+	/// <category>Toolkit</category>
+	/// <subcategory>ScaleLine</subcategory>
+	/// <usesoffline>false</usesoffline>
+	/// <usesonline>true</usesonline>
+	public partial class ScaleLineStylingSample : UserControl
+	{
+		public ScaleLineStylingSample()
+		{
+			InitializeComponent();
+		}
+	}
+
+	public class MultiplicationConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null || (value is string && string.IsNullOrEmpty(value as string)))
+				return value;
+			double d = System.Convert.ToDouble(value);
+			double frac = System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+			double ret = d * frac;
+			if (targetType == typeof(double))
+			{
+				return ret;
+			}
+			if (targetType == typeof(float))
+			{
+				return (float)ret;
+			}
+			if (targetType == typeof(int))
+			{
+				return (int)Math.Round(ret);
+			}
+			if (targetType == typeof(string))
+			{
+				return ret.ToString(System.Globalization.CultureInfo.InvariantCulture);
+			}
+			throw new NotSupportedException(string.Format("Conversion to {0} not supported", targetType));
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ samples/WinDesktop/ArcGISRuntime.Toolkit.Samples/TemplatePicker/TemplatePickerSample.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Esri.ArcGISRuntime.ArcGISServices;
+using Esri.ArcGISRuntime.Controls;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Layers;
+using Esri.ArcGISRuntime.Symbology;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace ArcGISRuntime.Toolkit.Samples.Desktop.TemplatePicker
+{
+	/// <summary>
+	/// Demonstrates how to use TemplatePicker control.
+	/// </summary>
+	/// <title>TemplatePicker</title>
+	/// <category>Toolkit</category>
+	/// <subcategory>TemplatePicker</subcategory>
+	/// <usesoffline>false</usesoffline>
+	/// <usesonline>true</usesonline>
+	public partial class TemplatePickerSample : UserControl
+	{
+		private FeatureTemplate _selectedTemplate; 
+
+		public TemplatePickerSample()
+		{
+			InitializeComponent();
+			MyMapView.NavigationCompleted += MyMapView_NavigationCompleted;
+		}
+
+		void MyMapView_NavigationCompleted(object sender, EventArgs e)
+		{
+			Debug.WriteLine(MyMapView.Extent.ToString());
+		}
+
+		private async void MyTemplatePicker_TemplatePicked(object sender, Esri.ArcGISRuntime.Toolkit.Controls.TemplatePicker.TemplatePickedEventArgs e)
+		{
+			try
+			{
+				TemplateName.Text = e.FeatureTemplate.Name;
+				TargetedLayer.Text = e.Layer.DisplayName;
+				TemplateDescription.Text = e.FeatureTemplate.Description;
+
+				GeometryType geometryType = GeometryType.Unknown;
+				var gdbFeatureTable = e.Layer.FeatureTable as GeodatabaseFeatureTable;
+				if (gdbFeatureTable != null && gdbFeatureTable.ServiceInfo != null)
+					geometryType = gdbFeatureTable.ServiceInfo.GeometryType;
+
+				Symbol symbol = null;
+
+				// Get symbol from the renderer if that is defined
+				if (e.Layer.Renderer != null)
+				{
+					var g = new Graphic(e.FeatureTemplate.Prototype.Attributes ??
+						Enumerable.Empty<KeyValuePair<string, object>>());
+
+					symbol = e.Layer.Renderer.GetSymbol(g);
+					SelectedSymbol.Source = await symbol.CreateSwatchAsync(32, 32, 96, Colors.Transparent, geometryType);
+				}
+
+				// Define what we shape we request from the editor
+				DrawShape requestedShape = DrawShape.Point;
+				if (e.FeatureTemplate.DrawingTool == FeatureEditTool.Polygon)
+					requestedShape = DrawShape.Polygon;
+				if (e.FeatureTemplate.DrawingTool == FeatureEditTool.Line)
+					requestedShape = DrawShape.Polyline;
+
+				// Request location for the new feature
+				var addedGeometry = await MyMapView.Editor.RequestShapeAsync(requestedShape, symbol);
+
+				// Add new feature to the MapView. In call gdbFeatureTable.ApplyEdits to commit changes to the service
+				var id = await gdbFeatureTable.AddAsync(e.FeatureTemplate.Prototype.Attributes, addedGeometry);
+			}
+			catch (TaskCanceledException) { } // Editing canceled
+			catch (Exception exception)
+			{
+				MessageBox.Show(string.Format("Error occured : {0}", exception.ToString()), "Sample error");
+			}
+		}
+	}
+}
