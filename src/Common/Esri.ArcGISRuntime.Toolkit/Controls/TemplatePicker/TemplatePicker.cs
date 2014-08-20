4457f9f37e9e97f7e4c827f1ce9f38f5026e79fa
mort5161 <mnielsen@esri.com>
2014-08-20 14:19:56 -0700

    Renamed GeodatabaseFeatureTable to ArcGISFeatureTable

--- src/Common/Esri.ArcGISRuntime.Toolkit/Controls/TemplatePicker/TemplatePicker.cs
+++ src/Common/Esri.ArcGISRuntime.Toolkit/Controls/TemplatePicker/TemplatePicker.cs
@@ -121,7 +121,7 @@ namespace Esri.ArcGISRuntime.Toolkit.Controls
         {
             var templates = new List<TemplateItem>();
             FeatureServiceLayerInfo serviceInfo = null;
-            var gdbFeatureTable = flayer.FeatureTable as GeodatabaseFeatureTable;
+            var gdbFeatureTable = flayer.FeatureTable as ArcGISFeatureTable;
             if (gdbFeatureTable != null && !gdbFeatureTable.IsReadOnly && flayer.Status == LayerStatus.Initialized)
             {
                 try
