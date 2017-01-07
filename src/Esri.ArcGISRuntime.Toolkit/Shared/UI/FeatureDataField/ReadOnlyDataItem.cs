d904c594af9b389b66c93654aaf4cd737f667c3b
Jennifer Nery <jnery@esri.com>
2017-01-06 17:52:19 -0800

    Added initial implementation of FeatureDataField to toolkit and a sample.

---
+++ src/Esri.ArcGISRuntime.Toolkit/Shared/UI/FeatureDataField/ReadOnlyDataItem.cs
@@ -0,0 +1,52 @@
+﻿// /*******************************************************************************
+//  * Copyright 2012-2016 Esri
+//  *
+//  *  Licensed under the Apache License, Version 2.0 (the "License");
+//  *  you may not use this file except in compliance with the License.
+//  *  You may obtain a copy of the License at
+//  *
+//  *  http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  *   Unless required by applicable law or agreed to in writing, software
+//  *   distributed under the License is distributed on an "AS IS" BASIS,
+//  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  *   See the License for the specific language governing permissions and
+//  *   limitations under the License.
+//  ******************************************************************************/
+
+using System;
+using Esri.ArcGISRuntime.Data;
+
+namespace Esri.ArcGISRuntime.Toolkit.UI
+{
+    /// <summary>
+    /// ReadOnlyDataItem is a bindable object for <see cref="FeatureDataField.ReadOnlyTemplate"/>.
+    /// </summary>
+    internal sealed class ReadOnlyDataItem : DataItem
+    {
+        private readonly Field _field;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadOnlyDataItem" /> class.
+        /// </summary>
+        /// <param name="value">default value.</param>
+        /// <param name="field">contains schema for field value.</param>
+        internal ReadOnlyDataItem(object value, Field field)
+            : base(null, value)
+        {
+            _value = value;
+            _field = field;
+        }
+
+        private object _value;
+
+        /// <summary>
+        /// Gets or sets the current attribute value of field.
+        /// </summary>
+        public override object Value
+        {
+            get { return _field?.GetDisplayValue(_value) ?? _value; }
+            set { _value = value; }
+        }
+    }
+}
---
+++ src/Samples/Toolkit.SampleApp.UWP/FeatureDataFieldSample.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Esri.ArcGISRuntime.Data;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Esri.ArcGISRuntime.Toolkit.Samples
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class FeatureDataFieldSample : Page
+    {
+        public FeatureDataFieldSample()
+        {
+            this.InitializeComponent();
+        }
+
+        private async Task LoadFeaturesAsync()
+        {
+            string error = null;
+            try
+            {
+                var table = new ServiceFeatureTable(new Uri("http://sampleserver6.arcgisonline.com/arcgis/rest/services/DamageAssessment/FeatureServer/0"));
+                table.FeatureRequestMode = FeatureRequestMode.ManualCache;
+                await table.LoadAsync();
+                var queryParameters = new QueryParameters() { WhereClause = "incidentid <> ''", MaxFeatures = 100 };
+                var outFields = new string[] { "objectid", "incidentid", "typdamage", "habitable", "predisval", "inspdate", "lastupdate" };
+                var assessments = await table.PopulateFromServiceAsync(queryParameters, true, outFields);
+                FeatureList.ItemsSource = assessments.ToList();
+            }
+            catch (Exception exception)
+            {
+                error = exception.Message;
+            }
+            if(!string.IsNullOrEmpty(error))
+            {
+                await new MessageDialog($"Error occured : {error}", "Sample error").ShowAsync();
+            }
+        }
+
+        private async void LoadFeatures_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await LoadFeaturesAsync();
+        }
+    }
+
+    public class ValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
