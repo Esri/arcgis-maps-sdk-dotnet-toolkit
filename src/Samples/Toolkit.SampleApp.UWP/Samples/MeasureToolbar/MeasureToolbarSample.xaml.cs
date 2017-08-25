c296301eabc914c54107dbb6215fd086c6c5aa88
Jennifer Nery <jnery@esri.com>
2017-08-24 17:52:16 -0700

    J nery/issue1412 measure tool (#140)
    * Initial implementation of MeasureToolbar.
    
    * Fixed identify for layers and overlays. Added code summaries. Added tooltip and fixed width for the units selector.
    
    * Added images and style for buttons. Updated minimum required template part. ClearGeometry - clears geometry instead of canceling editor.
    
    * Updated text alignment.
    
    * (UWP) Updated image for dark theme and resized image to match scale, replaced Undo/Redo/Clear with Segoe MDL2 Assets text. (WPF) Renamed image without scale, replaced Undo/Redo/Clear with Wingdings text. Fixed Measure Feature when identify results to nothing.
    
    * Added GraphicsOverlay for MeasureFeature only when there's highlighted feature in MeasureFeature mode; otherwise, removes from MapView.GraphicsOverlays collection. Clear will not remove result overlay until switching to a different mode. Switching to measure feature will not add result overlay until a feature is identified.

---
+++ src/Samples/Toolkit.SampleApp.UWP/Samples/MeasureToolbar/MeasureToolbarSample.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI;
+using System;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Esri.ArcGISRuntime.Toolkit.SampleApp.Samples.MeasureToolbar
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MeasureToolbarSample : Page
+    {
+        public MeasureToolbarSample()
+        {
+            this.InitializeComponent();
+
+            // Builds a map with different types of layer and graphic that can be identified for measure.
+
+            var map = new Map(Basemap.CreateLightGrayCanvasVector());
+
+            map.OperationalLayers.Add(new ArcGISMapImageLayer(new Uri("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer")));
+
+            map.OperationalLayers.Add(new FeatureLayer(new Uri("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Wildfire/FeatureServer/2")));
+            map.OperationalLayers.Add(new FeatureLayer(new Uri("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Wildfire/FeatureServer/1")));
+
+            mapView.Map = map;
+            AddRandomGraphics();
+        }
+
+        private void AddRandomGraphics()
+        {
+            var random = new Random();
+            var overlay = mapView.GraphicsOverlays.FirstOrDefault();
+            if (overlay != null)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    var mp = new MapPoint(random.Next(-180, 180), random.Next(-90, 90), SpatialReferences.Wgs84);
+                    var geometry = GeometryEngine.Buffer(mp, random.Next(1, 10));
+                    overlay.Graphics.Add(new Graphic(geometry));
+                }
+            }
+        }
+    }
+}
