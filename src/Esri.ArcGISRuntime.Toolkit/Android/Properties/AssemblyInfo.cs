c8300acdf74030e6a2a819a4bed85385deae5774
Rich Zwaap <rzwaap@esri.com>
2017-09-06 10:09:32 -0400

    Add Xamarin Android project

---
+++ src/Esri.ArcGISRuntime.Toolkit/Android/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Esri.ArcGISRuntime.Toolkit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Esri")]
+[assembly: AssemblyProduct("Esri.ArcGISRuntime.Toolkit")]
+[assembly: AssemblyCopyright("Copyright ©Esri 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
