d76b3dc9e02750e8bad6ba5fac044a33e5bd9ab2
dotMorten <no@spam.thx>
2017-12-13 16:00:05 -0800

    Fix iOS Sample app compilation issues, and add a mapView and ScaleLine

--- src/Samples/Toolkit.SampleApp.iOS/AppDelegate.cs
+++ src/Samples/Toolkit.SampleApp.iOS/AppDelegate.cs
@@ -4,7 +4,8 @@ using UIKit;
 namespace Esri.ArcGISRuntime.Toolkit.SampleApp
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the
-    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
     [Register("AppDelegate")]
     public class AppDelegate : UIApplicationDelegate
     {
@@ -18,14 +19,8 @@ namespace Esri.ArcGISRuntime.Toolkit.SampleApp
 
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
-            // create a new window instance based on the screen size
-            Window = new UIWindow(UIScreen.MainScreen.Bounds);
-
-            // If you have defined a root view controller, set it here:
-            // Window.RootViewController = myViewController;
-
-            // make the window visible
-            Window.MakeKeyAndVisible();
+            // Override point for customization after application launch.
+            // If not required for your application you can safely delete this method
 
             return true;
         }
@@ -62,5 +57,3 @@ namespace Esri.ArcGISRuntime.Toolkit.SampleApp
         }
     }
 }
-
-
---
+++ src/Samples/Toolkit.SampleApp.iOS/ViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace Esri.ArcGISRuntime.Toolkit.SampleApp
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            mapView.Map = new Mapping.Map(Mapping.Basemap.CreateLightGrayCanvasVector());
+            mapView.ViewpointChanged += (s, e) => { scaleLine.MapScale = mapView.MapScale; };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
