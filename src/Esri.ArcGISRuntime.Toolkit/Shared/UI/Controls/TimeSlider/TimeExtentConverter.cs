f4ff8252d9903a9e073213f6891bcb57b5308c52
Rich Zwaap <rich5785@esri.com>
2018-01-24 14:16:57 -0500

    Time Slider: code cleanup - usings and copyright header

--- src/Esri.ArcGISRuntime.Toolkit/Shared/UI/Controls/TimeSlider/TimeExtentConverter.cs
+++ src/Esri.ArcGISRuntime.Toolkit/Shared/UI/Controls/TimeSlider/TimeExtentConverter.cs
@@ -1,69 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// /*******************************************************************************
+//  * Copyright 2012-2018 Esri
+//  *
+//  *  Licensed under the Apache License, Version 2.0 (the "License");
+//  *  you may not use this file except in compliance with the License.
+//  *  You may obtain a copy of the License at
+//  *
+//  *  http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  *   Unless required by applicable law or agreed to in writing, software
+//  *   distributed under the License is distributed on an "AS IS" BASIS,
+//  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  *   See the License for the specific language governing permissions and
+//  *   limitations under the License.
+//  ******************************************************************************/
+
+using System;
 using System.ComponentModel;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Esri.ArcGISRuntime.Toolkit
 {
     /// <summary>
-    ///  *FOR INTERNAL USE ONLY* Provides a way of converting <see cref="TimeExtent"/> to and from a string representation.
+    /// *FOR INTERNAL USE ONLY* Provides a way of converting <see cref="TimeExtent"/> to and from a string representation.
     /// </summary>
     /// <exclude/>
     [EditorBrowsable(EditorBrowsableState.Never)]
     public class TimeExtentConverter : TypeConverter
     {
-        /// <summary>
-        ///     Returns whether this converter can convert an object of the given type to
-        /// the type of this converter, using the specified context.
-        /// </summary>
-        /// <param name="context">An System.ComponentModel.ITypeDescriptorContext that provides a format context.</param>
-        /// <param name="sourceType">A System.Type that represents the type you want to convert from.</param>
-        /// <returns>true if this converter can perform the conversion; otherwise, false.</returns>
+        /// <inheritdoc />
         public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
         {
             return sourceType == typeof(string) || sourceType == typeof(TimeExtent);
         }
 
-        /// <summary>
-        /// Returns whether this converter can convert the object to the specified type,
-        /// using the specified context.
-        /// </summary>
-        /// <param name="context">An System.ComponentModel.ITypeDescriptorContext that provides a format context.</param>
-        /// <param name="destinationType">A System.Type that represents the type you want to convert to.</param>
-        /// <returns>Returns whether this converter can convert the object to the specified type.</returns>
+        /// <inheritdoc />
         public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
         {
             return destinationType == typeof(string) || destinationType == typeof(TimeExtent) || base.CanConvertTo(context, destinationType);
         }
 
-        /// <summary>
-        /// Converts the given object to the type of this converter, using the specified
-        /// context and culture information.
-        /// </summary>
-        /// <param name="context">An System.ComponentModel.ITypeDescriptorContext that provides a format context.</param>
-        /// <param name="culture">The System.Globalization.CultureInfo to use as the current culture.</param>
-        /// <param name="value">The System.Object to convert.</param>
-        /// <returns>An System.Object that represents the converted value.</returns>
-        /// <exception cref="System.NotSupportedException">The conversion cannot be performed.</exception>
+        /// <inheritdoc />
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
         {
-            return ConvertFromString(value as string);
+            return Convert(value as string);
         }
 
-        /// <summary>
-        /// Converts the given value object to the specified type, using the specified
-        /// context and culture information.
-        /// </summary>
-        /// <param name="context">An System.ComponentModel.ITypeDescriptorContext that provides a format context.</param>
-        /// <param name="culture">A System.Globalization.CultureInfo. If null is passed, the current culture is assumed.</param>
-        /// <param name="value">The System.Object to convert.</param>
-        /// <param name="destinationType">The System.Type to convert the value parameter to.</param>
-        /// <returns>An System.Object that represents the converted value.</returns>
-        /// <exception cref="System.NotSupportedException">The conversion cannot be performed.</exception>
-        /// <exception cref="System.ArgumentNullException">The destinationType parameter is null.</exception>
+        /// <inheritdoc />
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
         {
             if (destinationType == null)
@@ -72,13 +54,12 @@ namespace Esri.ArcGISRuntime.Toolkit
             }
             if (destinationType == typeof(string) && value is TimeExtent)
             {
-                return ConvertToString((TimeExtent)value);
+                return Convert((TimeExtent)value);
             }
             return base.ConvertTo(context, culture, value, destinationType);
         }
 
-
-        private TimeExtent ConvertFromString(string timeExtentString)
+        private TimeExtent Convert(string timeExtentString)
         {
             if (string.IsNullOrEmpty(timeExtentString))
                 return null;
@@ -101,7 +82,7 @@ namespace Esri.ArcGISRuntime.Toolkit
             return timeExtent;
         }
 
-        private string ConvertToString(TimeExtent timeExtent)
+        private string Convert(TimeExtent timeExtent)
         {
             if (timeExtent == null)
                 return null;
