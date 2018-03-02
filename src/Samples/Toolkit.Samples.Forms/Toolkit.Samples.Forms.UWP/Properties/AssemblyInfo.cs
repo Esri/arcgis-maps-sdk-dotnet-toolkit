944028cf00712e4afb78421ffa17fc22ac6fbe24
dotMorten <no@spam.thx>
2018-03-02 11:48:33 -0800

    Added a simple Xamarin.Forms sample app

---
+++ src/Samples/Toolkit.Samples.Forms/Toolkit.Samples.Forms.UWP/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Toolkit.Samples.Forms.UWP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Toolkit.Samples.Forms.UWP")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
