bfa57c2b1d2a74780de597c169098f3bf146bed4
Rich Zwaap <rzwaap@esri.com>
2018-03-28 21:59:03 -0400

    iOS Time Slider: Remove unused Dispatcher members

--- src/Esri.ArcGISRuntime.Toolkit/Esri.ArcGISRuntime.Toolkit/Internal/Dispatcher.cs
+++ src/Esri.ArcGISRuntime.Toolkit/Esri.ArcGISRuntime.Toolkit/Internal/Dispatcher.cs
@@ -32,39 +32,6 @@ namespace Esri.ArcGISRuntime.Toolkit.Internal
     /// </summary>
     internal class Dispatcher
     {
-        // Exclude Dispatcher.CheckAccess from Android (for now) so we don't use it without considering its
-        // implications.  When this is called, native Java Looper instances are created that map to the managed
-        // Looper representations.  Normally this is fine, but when called in rapid succession, the peer Java
-        // instances can apparently get out of sync with their managed counterparts, and this seems further
-        // exacerbated if there is asynchrony involved.  The end result is a broken broken CLR <--> JNI object
-        // linkage, leading to a deleted global JNI ref error and crash.  See:
-        // https://devtopia.esri.com/runtime/dotnet-api/issues/3765
-        // https://devtopia.esri.com/runtime/dotnet-api/issues/4516
-#if !__ANDROID__
-        /// <summary>
-        /// Check whether execution is currently running on the UI thread
-        /// </summary>
-        /// <returns>true if currently executing on UI thread, false otherwise</returns>
-        internal static bool CheckAccess()
-        {
-            var hasAccess = true;
-
-#if __ANDROID__
-            using (Looper looper = Looper.MyLooper())
-            {
-                using (Looper mainLooper = Looper.MainLooper)
-                {
-                    hasAccess = looper == mainLooper;
-                }
-            }
-#elif __IOS__
-            hasAccess = NSThread.Current.IsMainThread;
-#endif
-
-            return hasAccess;
-        }
-#endif
-
 #if __ANDROID__
         // Create a static instance of this and re-use so that only one peer Java instance is created.
         private static Handler _handler = new Handler(Looper.MainLooper);
@@ -84,33 +51,6 @@ namespace Esri.ArcGISRuntime.Toolkit.Internal
 #elif __ANDROID__
             _handler.Post(a);
 #endif
-        }
-
-        /// <summary>
-        /// Executes the specified action on the UI thread asynchronously
-        /// </summary>
-        /// <returns>Task</returns>
-        /// <remarks>
-        /// - Uses Xamarin.Forms Device class to invoke action on the UI thread
-        /// - Returns a task that can be awaited if necessary
-        /// </remarks>
-        internal static Task InvokeAsync(Action a)
-        {
-            var tcs = new TaskCompletionSource<bool>();
-            Action action = () =>
-            {
-                try
-                {
-                    a();
-                    tcs.TrySetResult(true);
-                }
-                catch (Exception ex)
-                {
-                    tcs.TrySetException(ex);
-                }
-            };
-            RunAsyncAction(action);
-            return tcs.Task;
         }
     }
 }
