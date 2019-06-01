8547bf8ff7077e503a4e1be1cc0f495c612a6028
Morten Nielsen <mn@iter.dk>
2019-05-31 20:33:48 -0700

    Added symbol display sample to WPF

--- src/Samples/Toolkit.SampleApp.WPF/Samples/SymbolDisplay/SymbolDisplaySample.xaml.cs
+++ src/Samples/Toolkit.SampleApp.WPF/Samples/SymbolDisplay/SymbolDisplaySample.xaml.cs
@@ -24,11 +24,50 @@ namespace Esri.ArcGISRuntime.Toolkit.Samples.SymbolDisplay
         public SymbolDisplaySample()
         {
             InitializeComponent();
-            DataContext = this;
+            LoadSymbols();
         }
 
-        public List<SimpleMarkerSymbolStyle> SimpleMarkerSymbolStyles { get; } = Enum.GetValues(typeof(SimpleMarkerSymbolStyle)).OfType<SimpleMarkerSymbolStyle>().ToList();
+        private void LoadSymbols()
+        {
+            AddSymbol(new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, System.Drawing.Color.White, 10));
+            AddSymbol(new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, System.Drawing.Color.White, 20));
+            AddSymbol(new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, System.Drawing.Color.White, 30));
+            AddSymbol(new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, System.Drawing.Color.White, 40));
 
-        public SimpleMarkerSymbol Symbol { get; } = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Square, System.Drawing.Color.Red, 20) { Outline = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, System.Drawing.Color.Black, 2) };
+            AddSymbol(new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Diamond, System.Drawing.Color.Red, 10));
+            AddSymbol(new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Cross, System.Drawing.Color.Green, 20));
+            AddSymbol(new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Triangle, System.Drawing.Color.Blue, 30));
+            AddSymbol(new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.X, System.Drawing.Color.Blue, 40));
+
+            AddSymbol(new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, System.Drawing.Color.Orange, 5));
+            AddSymbol(new SimpleLineSymbol(SimpleLineSymbolStyle.DashDot, System.Drawing.Color.Orange, 2));
+            AddSymbol(new SimpleFillSymbol(SimpleFillSymbolStyle.Solid, System.Drawing.Color.Orange, new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, System.Drawing.Color.White, 2)));
+            AddSymbol(new SimpleFillSymbol(SimpleFillSymbolStyle.ForwardDiagonal, System.Drawing.Color.Yellow, new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, System.Drawing.Color.Yellow, 2)));
+
+            AddSymbol(new PictureMarkerSymbol(new Uri("https://cdn3.iconfinder.com/data/icons/web-and-internet-icons/512/Information-256.png")));
+        }
+
+        private void AddSymbol(Symbology.Symbol symbol)
+        {
+            int columnCount = SymbolGrid.ColumnDefinitions.Count;
+            var sd = new UI.Controls.SymbolDisplay() { Symbol = symbol };
+            var f = new Border()
+            {
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center,
+                BorderBrush = new SolidColorBrush(Colors.Black),
+                BorderThickness = new Thickness(1),
+                Padding = new Thickness(0)
+            };
+            f.Child = sd;
+            int count = SymbolGrid.Children.Count;
+            var row = count / columnCount;
+            var column = count % columnCount;
+            if (column == 0)
+                SymbolGrid.RowDefinitions.Add(new RowDefinition());
+            Grid.SetRow(f, row);
+            Grid.SetColumn(f, column);
+            SymbolGrid.Children.Add(f);
+        }
     }
 }
