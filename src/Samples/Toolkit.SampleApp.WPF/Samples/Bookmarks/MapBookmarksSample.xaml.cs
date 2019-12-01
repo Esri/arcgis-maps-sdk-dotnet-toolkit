c9bf2f62f6cae8455fe3e048b7dc41f98473cd2f
Nathan Castle <ncastle@esri.com>
2019-11-30 18:08:09 -0800

    Samples: add item template usage to tests WPF+UWP

--- src/Samples/Toolkit.SampleApp.WPF/Samples/Bookmarks/MapBookmarksSample.xaml.cs
+++ src/Samples/Toolkit.SampleApp.WPF/Samples/Bookmarks/MapBookmarksSample.xaml.cs
@@ -19,7 +19,7 @@ namespace Esri.ArcGISRuntime.Toolkit.Samples.Bookmarks
     /// <summary>
     /// Interaction logic for MapBookmarksSample.xaml
     /// </summary>
-    [SampleInfoAttribute(Category = "Bookmarks", DisplayName = "Bookmarks - MapView", Description = "Bookmarks driven by a mapview")]
+    [SampleInfoAttribute(Category = "Bookmarks", DisplayName = "Bookmarks - MapView", Description = "Bookmarks driven by a mapview with custom item template.")]
     public partial class MapBookmarksSample : UserControl
     {
         private const string _mapUrl = "https://arcgisruntime.maps.arcgis.com/home/webmap/viewer.html?webmap=1c45a922e9e7465295323f4d2e7e42ee";
