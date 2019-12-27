93732e1b3154a07453513fd49b27ef54484a0971
Nathan Castle <ncastle@esri.com>
2019-12-27 12:27:30 -0800

    Update UWP sample

--- src/Samples/Toolkit.SampleApp.UWP/Samples/BookmarksView/BookmarksViewSample.xaml.cs
+++ src/Samples/Toolkit.SampleApp.UWP/Samples/BookmarksView/BookmarksViewSample.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI.Controls;
 using System;
 using System.Collections.ObjectModel;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -13,59 +16,127 @@ namespace Esri.ArcGISRuntime.Toolkit.SampleApp.Samples.BookmarksView
     /// </summary>
     public sealed partial class BookmarksViewSample : Page
     {
-        private readonly string[] _mapUrl = new[]
-        {
-            "https://arcgisruntime.maps.arcgis.com/home/webmap/viewer.html?webmap=1c45a922e9e7465295323f4d2e7e42ee",
-            "https://runtime.maps.arcgis.com/home/item.html?id=16f1b8ba37b44dc3884afc8d5f454dd2",
-            "https://arcgisruntime.maps.arcgis.com/home/webmap/viewer.html?webmap=e50fafe008ac4ce4ad2236de7fd149c3"
-        };
-        private int _mapIndex = 0;
+        private const string webMapOneUrl = "https://arcgisruntime.maps.arcgis.com/home/item.html?id=e50fafe008ac4ce4ad2236de7fd149c3";
+        private const string webMapTwoUrl = "https://arcgisruntime.maps.arcgis.com/home/item.html?id=16f1b8ba37b44dc3884afc8d5f454dd2";
+        private const string webSceneOne = "https://arcgisruntime.maps.arcgis.com/home/item.html?id=6b6588041965408e84ba319e12d9d7ad";
+        private const string webSceneTwo = "https://arcgisruntime.maps.arcgis.com/home/item.html?id=c6e7476998c649b482849eb92b967761";
 
-        private ObservableCollection<Bookmark> _bookmarksObservable = new ObservableCollection<Bookmark>();
+        private Random _randomizer = new Random();
+
+        private ObservableCollection<Bookmark> bookmarksOverride = new ObservableCollection<Bookmark>
+        {
+            new Bookmark("Override One", new Viewpoint(11,11,1100)),
+            new Bookmark("Override Two", new Viewpoint(22,-22.22,1200)),
+            new Bookmark("Override Three", new Viewpoint(-33,33,3000)),
+            new Bookmark("Override Four", new Viewpoint(44,44,4400)),
+            new Bookmark("Override Five", new Viewpoint(-55,-55,5500)),
+        };
 
         public BookmarksViewSample()
         {
             InitializeComponent();
-            MyMapView.Map = new Map(new Uri(_mapUrl[0]));
-            InitializeLists();
         }
 
-        private void InitializeLists()
+        private void SetMapViewBinding_Click(object sender, RoutedEventArgs e)
         {
-            Viewpoint vp = new Viewpoint(0, 0, 1500, new Camera(0, 0, 150000, 60, 35, 20));
-            _bookmarksObservable.Add(new Bookmark("O: 0,0", vp));
-
-            Viewpoint vp2 = new Viewpoint(48.850684, 2.347735, 1000, new Camera(48.850684, 2.347735, 1500, 100, 35, 0));
-            _bookmarksObservable.Add(new Bookmark("O: Paris", vp2));
-
-            Viewpoint vp3 = new Viewpoint(48.034682, 13.710577, 1300, new Camera(48.034682, 13.710577, 2000, 100, 35, 0));
-            _bookmarksObservable.Add(new Bookmark("O: Pühret, Austria", vp3));
-
-
-            Viewpoint vp4 = new Viewpoint(47.787947, 16.755135, 1300, new Camera(47.787947, 16.755135, 3000, 100, 35, 0));
-            _bookmarksObservable.Add(new Bookmark("O: Nationalpark Neusiedler See - Seewinkel Informationszentrum", vp4));
+            MyMapView.Visibility = Visibility.Visible;
+            MySceneView.Visibility = Visibility.Collapsed;
+            Binding geoviewBinding = new Binding();
+            geoviewBinding.Source = MyMapView;
+            BookmarksView.SetBinding(UI.Controls.BookmarksView.GeoViewProperty, geoviewBinding);
         }
 
-        private void ShowObservableList_Click(object sender, RoutedEventArgs e) => MyBookmarks.BookmarksOverride = _bookmarksObservable;
-
-        private void RemoveFromObservableButton_Click(object sender, RoutedEventArgs e) => _bookmarksObservable.RemoveAt(0);
-
-        private void AddToObservableButton_Click(object sender, RoutedEventArgs e)
+        private void SetSceneViewBinding_Click(object sender, RoutedEventArgs e)
         {
-            Viewpoint vp5 = new Viewpoint(47.787947, 16.755135, 1300, new Camera(47.787947, 16.755135, 3000, 100, 35, 0));
-
-            _bookmarksObservable.Add(new Bookmark($"O: {_bookmarksObservable.Count}", vp5));
+            MyMapView.Visibility = Visibility.Collapsed;
+            MySceneView.Visibility = Visibility.Visible;
+            Binding geoviewBinding = new Binding();
+            geoviewBinding.Source = MySceneView;
+            BookmarksView.SetBinding(UI.Controls.BookmarksView.GeoViewProperty, geoviewBinding);
         }
 
-        private void SwitchMapButton_Click(object sender, RoutedEventArgs e) => MyMapView.Map = new Map(new Uri(_mapUrl[++_mapIndex % _mapUrl.Length]));
-
-        private void RemoveListOverrideButton_Click(object sender, RoutedEventArgs e) => MyBookmarks.BookmarksOverride = null;
-
-        private void AddMapBookmarkButton_Click(object sender, RoutedEventArgs e)
+        private void SetDocumentOne_Click(object sender, RoutedEventArgs e)
         {
-            Viewpoint vp5 = new Viewpoint(47.787947, 16.755135, 1300, new Camera(47.787947, 16.755135, 3000, 100, 35, 0));
+            if (BookmarksView.GeoView is MapView mapView)
+            {
+                mapView.Map = new Map(new Uri(webMapOneUrl));
+            }
+            else if (BookmarksView.GeoView is SceneView sceneView)
+            {
+                sceneView.Scene = new Scene(new Uri(webSceneOne));
+            }
+        }
 
-            MyMapView.Map.Bookmarks.Add(new Bookmark($"M: {MyMapView.Map.Bookmarks.Count}", vp5));
+        private void SetDocumentTwo_Click(object sender, RoutedEventArgs e)
+        {
+            if (BookmarksView.GeoView is MapView mapView)
+            {
+                mapView.Map = new Map(new Uri(webMapTwoUrl));
+            }
+            else if (BookmarksView.GeoView is SceneView sceneView)
+            {
+                sceneView.Scene = new Scene(new Uri(webSceneTwo));
+            }
+        }
+
+        private void SetOverrideList_Click(object sender, RoutedEventArgs e)
+        {
+            BookmarksView.BookmarksOverride = bookmarksOverride;
+        }
+
+        private void ClearOverrideList_Click(object sender, RoutedEventArgs e)
+        {
+            BookmarksView.BookmarksOverride = null;
+        }
+
+        private void AddToOverrideList_Click(object sender, RoutedEventArgs e)
+        {
+            double longitude = (_randomizer.NextDouble() * 360) - 180;
+            double latitude = (_randomizer.NextDouble() * 180) - 90;
+
+            bookmarksOverride.Add(new Bookmark($"BM {bookmarksOverride.Count}", new Viewpoint(latitude, longitude, 2375)));
+        }
+
+        private void AddToMapScene_Click(object sender, RoutedEventArgs e)
+        {
+            double longitude = (_randomizer.NextDouble() * 360) - 180;
+            double latitude = (_randomizer.NextDouble() * 180) - 90;
+
+            if (BookmarksView.GeoView is MapView mapView)
+            {
+                mapView.Map.Bookmarks.Add(new Bookmark($"doc {mapView.Map.Bookmarks.Count}", new Viewpoint(latitude, longitude, 2375)));
+            }
+            else if (BookmarksView.GeoView is SceneView sceneView)
+            {
+                sceneView.Scene.Bookmarks.Add(new Bookmark($"doc {sceneView.Scene.Bookmarks.Count}", new Viewpoint(latitude, longitude, 2375)));
+            }
+        }
+
+        private void SetItemTemplateOne_Click(object sender, RoutedEventArgs e)
+        {
+            DataTemplate template = Resources["ItemTemplateOne"] as DataTemplate;
+            BookmarksView.ItemTemplate = template;
+        }
+
+        private void SetItemTemplateTwo_Click(object sender, RoutedEventArgs e)
+        {
+            DataTemplate template = Resources["ItemTemplateTwo"] as DataTemplate;
+            BookmarksView.ItemTemplate = template;
+        }
+
+        private void AddSelectionListener_Click(object sender, RoutedEventArgs e)
+        {
+            BookmarksView.BookmarkSelected += BookmarkSelected;
+        }
+
+        private void RemoveSelectionListener_Click(object sender, RoutedEventArgs e)
+        {
+            BookmarksView.BookmarkSelected -= BookmarkSelected;
+        }
+
+        private async void BookmarkSelected(object sender, UI.BookmarkSelectedEventArgs e)
+        {
+            await new MessageDialog($"{e.Bookmark.Name} Selected!").ShowAsync();
         }
     }
 }
