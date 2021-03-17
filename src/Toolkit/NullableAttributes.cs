9c2333deb59c0d8096f5e3657058e2c552897891
Morten Nielsen <mn@iter.dk>
2021-03-17 11:56:20 -0700

    Add more nullable annotations

--- src/Toolkit/NullableAttributes.cs
+++ src/Toolkit/NullableAttributes.cs
@@ -25,56 +25,59 @@
 #pragma warning disable SA1623 // Property summary documentation should match accessors
 #pragma warning disable SA1502 // Element should not be on a single line
 
-// Source: https://github.com/dotnet/corefx/blob/b2097cbdcb26f7f317252334ddcce101a20b7f3d/src/Common/src/CoreLib/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
+// Source: https://github.com/dotnet/runtime/blob/4e3deb5100c815e880403a7cf31304a616db4e10/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
 
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 namespace System.Diagnostics.CodeAnalysis
 {
     /// <summary>Specifies that null is allowed as an input even if the corresponding type disallows it.</summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
-#if INTERNAL_NULLABLE_ATTRIBUTES
-    internal
-#else
+#if SYSTEM_PRIVATE_CORELIB
     public
+#else
+    internal
 #endif
-        sealed class AllowNullAttribute : Attribute { }
+        sealed class AllowNullAttribute : Attribute
+    { }
 
     /// <summary>Specifies that null is disallowed as an input even if the corresponding type allows it.</summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
-#if INTERNAL_NULLABLE_ATTRIBUTES
-    internal
-#else
+#if SYSTEM_PRIVATE_CORELIB
     public
+#else
+    internal
 #endif
-        sealed class DisallowNullAttribute : Attribute { }
+        sealed class DisallowNullAttribute : Attribute
+    { }
 
     /// <summary>Specifies that an output may be null even if the corresponding type disallows it.</summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
-#if INTERNAL_NULLABLE_ATTRIBUTES
-    internal
-#else
+#if SYSTEM_PRIVATE_CORELIB
     public
+#else
+    internal
 #endif
-        sealed class MaybeNullAttribute : Attribute { }
+        sealed class MaybeNullAttribute : Attribute
+    { }
 
-    /// <summary>Specifies that an output will not be null even if the corresponding type allows it.</summary>
+    /// <summary>Specifies that an output will not be null even if the corresponding type allows it. Specifies that an input argument was not null when the call returns.</summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
-#if INTERNAL_NULLABLE_ATTRIBUTES
-    internal
-#else
+#if SYSTEM_PRIVATE_CORELIB
     public
+#else
+    internal
 #endif
-        sealed class NotNullAttribute : Attribute { }
+        sealed class NotNullAttribute : Attribute
+    { }
 
     /// <summary>Specifies that when a method returns <see cref="ReturnValue"/>, the parameter may be null even if the corresponding type disallows it.</summary>
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-#if INTERNAL_NULLABLE_ATTRIBUTES
-    internal
-#else
+#if SYSTEM_PRIVATE_CORELIB
     public
+#else
+    internal
 #endif
         sealed class MaybeNullWhenAttribute : Attribute
     {
@@ -90,10 +93,10 @@ namespace System.Diagnostics.CodeAnalysis
 
     /// <summary>Specifies that when a method returns <see cref="ReturnValue"/>, the parameter will not be null even if the corresponding type allows it.</summary>
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-#if INTERNAL_NULLABLE_ATTRIBUTES
-    internal
-#else
+#if SYSTEM_PRIVATE_CORELIB
     public
+#else
+    internal
 #endif
         sealed class NotNullWhenAttribute : Attribute
     {
@@ -109,10 +112,10 @@ namespace System.Diagnostics.CodeAnalysis
 
     /// <summary>Specifies that the output will be non-null if the named parameter is non-null.</summary>
     [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
-#if INTERNAL_NULLABLE_ATTRIBUTES
-    internal
-#else
+#if SYSTEM_PRIVATE_CORELIB
     public
+#else
+    internal
 #endif
         sealed class NotNullIfNotNullAttribute : Attribute
     {
@@ -128,19 +131,20 @@ namespace System.Diagnostics.CodeAnalysis
 
     /// <summary>Applied to a method that will never return under any circumstance.</summary>
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
-#if INTERNAL_NULLABLE_ATTRIBUTES
-    internal
-#else
+#if SYSTEM_PRIVATE_CORELIB
     public
+#else
+    internal
 #endif
-        sealed class DoesNotReturnAttribute : Attribute { }
+        sealed class DoesNotReturnAttribute : Attribute
+    { }
 
     /// <summary>Specifies that the method will not return if the associated Boolean parameter is passed the specified value.</summary>
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
-#if INTERNAL_NULLABLE_ATTRIBUTES
-    internal
-#else
+#if SYSTEM_PRIVATE_CORELIB
     public
+#else
+    internal
 #endif
         sealed class DoesNotReturnIfAttribute : Attribute
     {
@@ -153,6 +157,73 @@ namespace System.Diagnostics.CodeAnalysis
 
         /// <summary>Gets the condition parameter value.</summary>
         public bool ParameterValue { get; }
+    }
+
+    /// <summary>Specifies that the method or property will ensure that the listed field and property members have not-null values.</summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+#if SYSTEM_PRIVATE_CORELIB
+    public
+#else
+    internal
+#endif
+        sealed class MemberNotNullAttribute : Attribute
+    {
+        /// <summary>Initializes the attribute with a field or property member.</summary>
+        /// <param name="member">
+        /// The field or property member that is promised to be not-null.
+        /// </param>
+        public MemberNotNullAttribute(string member) => Members = new[] { member };
+
+        /// <summary>Initializes the attribute with the list of field and property members.</summary>
+        /// <param name="members">
+        /// The list of field and property members that are promised to be not-null.
+        /// </param>
+        public MemberNotNullAttribute(params string[] members) => Members = members;
+
+        /// <summary>Gets field or property member names.</summary>
+        public string[] Members { get; }
+    }
+
+    /// <summary>Specifies that the method or property will ensure that the listed field and property members have not-null values when returning with the specified return value condition.</summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+#if SYSTEM_PRIVATE_CORELIB
+    public
+#else
+    internal
+#endif
+        sealed class MemberNotNullWhenAttribute : Attribute
+    {
+        /// <summary>Initializes the attribute with the specified return value condition and a field or property member.</summary>
+        /// <param name="returnValue">
+        /// The return value condition. If the method returns this value, the associated parameter will not be null.
+        /// </param>
+        /// <param name="member">
+        /// The field or property member that is promised to be not-null.
+        /// </param>
+        public MemberNotNullWhenAttribute(bool returnValue, string member)
+        {
+            ReturnValue = returnValue;
+            Members = new[] { member };
+        }
+
+        /// <summary>Initializes the attribute with the specified return value condition and list of field and property members.</summary>
+        /// <param name="returnValue">
+        /// The return value condition. If the method returns this value, the associated parameter will not be null.
+        /// </param>
+        /// <param name="members">
+        /// The list of field and property members that are promised to be not-null.
+        /// </param>
+        public MemberNotNullWhenAttribute(bool returnValue, params string[] members)
+        {
+            ReturnValue = returnValue;
+            Members = members;
+        }
+
+        /// <summary>Gets the return value condition.</summary>
+        public bool ReturnValue { get; }
+
+        /// <summary>Gets field or property member names.</summary>
+        public string[] Members { get; }
     }
 }
 #pragma warning restore SA1502 // Element should not be on a single line
