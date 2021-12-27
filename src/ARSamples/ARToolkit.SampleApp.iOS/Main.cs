2320cb1d81644be14846b990a006833f17d8bb26
Nathan Castle <ncastle@esri.com>
2021-12-27 13:32:42 -0800

    iOS Samples: use correct UIApplication.Main impl
    Previously-used API was deprecated in a recent version of Xamarin.iOS.

--- src/ARSamples/ARToolkit.SampleApp.iOS/Main.cs
+++ src/ARSamples/ARToolkit.SampleApp.iOS/Main.cs
@@ -13,7 +13,7 @@ namespace ARToolkit.SampleApp
 
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
 }
