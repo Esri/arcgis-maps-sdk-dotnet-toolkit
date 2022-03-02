abee536e9a4c22b182d9f64469bb3ceef3a575d7
Nathan Castle <ncastle@esri.com>
2022-03-01 18:58:29 -0800

    Implement selection notification, add test/sample

--- src/Samples/Toolkit.SampleApp.WPF_Net5/Samples/SearchView/SearchViewCustomizationSample.xaml.cs
+++ src/Samples/Toolkit.SampleApp.WPF_Net5/Samples/SearchView/SearchViewCustomizationSample.xaml.cs
@@ -1,8 +1,15 @@
+using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.Tasks.Geocoding;
 using Esri.ArcGISRuntime.Toolkit.UI.Controls;
+using Esri.ArcGISRuntime.UI;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -89,6 +96,67 @@ namespace Esri.ArcGISRuntime.Toolkit.Samples.SearchView
                     MySearchView.SearchViewModel.SearchMode = SearchResultMode.Multiple;
                     break;
             }
+        }
+
+        private void AddTestLocator_Click(object sender, RoutedEventArgs e)
+        {
+            MySearchView.SearchViewModel.Sources.Add(new TestSearchSource());
+        }
+
+        private class TestSearchSource : ISearchSource
+        {
+            public string DisplayName { get => "Event tester"; set => throw new NotImplementedException(); }
+            public string Placeholder { get => "Test placeholder"; set => throw new NotImplementedException(); }
+            public CalloutDefinition DefaultCalloutDefinition { get => null; set => throw new NotImplementedException(); }
+            public Symbol DefaultSymbol { get => null; set => throw new NotImplementedException(); }
+            public double DefaultZoomScale { get => 1000; set => throw new NotImplementedException(); }
+            public int MaximumResults { get => 3; set => throw new NotImplementedException(); }
+            public int MaximumSuggestions { get => 3; set => throw new NotImplementedException(); }
+            public Geometry.Geometry SearchArea { get => null; set { } }
+            public MapPoint PreferredSearchLocation { get => null; set { } }
+
+            public void NotifyDeselected(SearchResult result)
+            {
+                MessageBox.Show($"Deselected {result?.DisplayTitle ?? "all results"}");
+            }
+
+            public void NotifySelected(SearchResult result)
+            {
+                MessageBox.Show($"Selected {result.DisplayTitle}");
+            }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+            public async Task<IList<SearchResult>> RepeatSearchAsync(string queryString, Envelope queryExtent, CancellationToken cancellationToken)
+            {
+                var list = new [] {"one", "two", "three", "four"};
+                return list.Select(m => new SearchResult($"repeat {m}", "repeat subtitle", this, null, null)).ToList();
+            }
+
+            public async Task<IList<SearchResult>> SearchAsync(string queryString, CancellationToken cancellationToken)
+            {
+                var list = new [] {"one", "two", "three", "four"};
+                return list.Select(m => new SearchResult($"explicit search {m}", "repeat subtitle", this, null, null)).ToList();
+            }
+
+            public async Task<IList<SearchResult>> SearchAsync(SearchSuggestion suggestion, CancellationToken cancellationToken)
+            {
+                if (suggestion.IsCollection)
+                {
+                    var list = new [] {"one", "two", "three", "four"};
+                    return list.Select(m => new SearchResult($"search from suggestion - res: {m}", suggestion.DisplayTitle, this, null, null)).ToList();
+                }
+                else
+                {
+                    return new List<SearchResult>() { new SearchResult($"result from suggestion {suggestion.DisplayTitle}", "from suggestion", this, null, null)};
+                }
+            }
+
+            public async Task<IList<SearchSuggestion>> SuggestAsync(string queryString, CancellationToken cancellationToken)
+            {
+                var list = new [] {"one", "two", "three", "four"};
+                return list.Select(m => new SearchSuggestion($"suggestion {m}", this) { IsCollection = m.Contains("w")}).ToList();
+            }
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         }
     }
 }
