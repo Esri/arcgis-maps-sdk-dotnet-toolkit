b0b77fd743fb3757252c8dee0729955d0d964234
Morten Nielsen <mn@iter.dk>
2022-08-22 09:24:58 -0700

    Add Maui builder

---
+++ src/Toolkit/Toolkit.Maui/AppHostBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Esri.ArcGISRuntime.Maui.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esri.ArcGISRuntime.Toolkit.Maui
+{
+    public static class AppHostBuilderExtensions
+    {
+        /// <summary>
+        ///  Initializes the ArcGIS Runtime Toolkit Controls.
+        /// </summary>
+        /// <param name="builder">The Maui host builder.</param>
+        /// <returns>The host builder</returns>
+        public static MauiAppBuilder UseArcGISToolkit(this MauiAppBuilder builder)
+        {
+            //builder.ConfigureMauiHandlers(delegate (IMauiHandlersCollection a)
+            //{
+            //    a.AddHandler(typeof(Compass), typeof(CompassHandler));
+            //});
+
+            return builder; 
+        }
+    }
+}
