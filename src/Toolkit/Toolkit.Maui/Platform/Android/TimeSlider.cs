2bc7a2d204df5b2b58050348dc7e390060655645
Nathan Castle <ncastle@esri.com>
2022-08-30 16:36:47 -0700

    WIP

---
+++ src/Toolkit/Toolkit.Maui/Platform/Android/TimeSlider.cs
@@ -0,0 +1,328 @@
+﻿// /*******************************************************************************
+//  * Copyright 2012-2018 Esri
+//  *
+//  *  Licensed under the Apache License, Version 2.0 (the "License");
+//  *  you may not use this file except in compliance with the License.
+//  *  You may obtain a copy of the License at
+//  *
+//  *  http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  *   Unless required by applicable law or agreed to in writing, software
+//  *   distributed under the License is distributed on an "AS IS" BASIS,
+//  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  *   See the License for the specific language governing permissions and
+//  *   limitations under the License.
+//  ******************************************************************************/
+
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using Android.Content;
+using Android.Graphics;
+using Android.Runtime;
+
+#if NET6_0_OR_GREATER
+using AndroidX.ConstraintLayout.Widget;
+#else
+using Android.Support.Constraints;
+#endif
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Esri.ArcGISRuntime.Toolkit.Internal;
+using Esri.ArcGISRuntime.Toolkit.Primitives;
+using Esri.ArcGISRuntime.Toolkit.Maui;
+using Esri.ArcGISRuntime.Toolkit.Maui.Platform.Android;
+using Esri.ArcGISRuntime.Toolkit.Properties;
+
+namespace Esri.ArcGISRuntime.Toolkit.UI.Controls
+{
+    [Register("Esri.ArcGISRuntime.Toolkit.UI.Controls.TimeSlider")]
+    [DisplayName("Time Slider")]
+    [Category("ArcGIS Runtime Controls")]
+    public partial class TimeSlider : ConstraintLayout, View.IOnTouchListener
+    {
+#pragma warning disable SX1309 // Names match elements in template
+#pragma warning disable SA1306 // Names match elements in template
+        private View? SliderTrack;
+        private View? SliderTrackOutline;
+        private View? MinimumThumb;
+        private View? MaximumThumb;
+        private View? PinnedMinimumThumb;
+        private View? PinnedMaximumThumb;
+        private View? HorizontalTrackThumb;
+        private Button? NextButton;
+        private Button? PreviousButton;
+        private View? NextButtonOutline;
+        private View? PreviousButtonOutline;
+        private ToggleButton? PlayPauseButton;
+        private View? PlayButtonOutline;
+        private View? PauseButtonOutline;
+        private RectangleView? SliderTrackStepBackRepeater = null;
+        private RectangleView? SliderTrackStepForwardRepeater = null;
+#pragma warning restore SX1309
+#pragma warning restore SA1306
+        private View? _startTimeTickmark;
+        private View? _endTimeTickmark;
+        private bool _isMinThumbFocused = false;
+        private bool _isMaxThumbFocused = false;
+        private float _lastX = 0;
+        private ThrottleAwaiter _measureThrottler = new ThrottleAwaiter(1);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimeSlider"/> class.
+        /// </summary>
+        /// <param name="context">The Context the view is running in, through which it can access resources, themes, etc.</param>
+        public TimeSlider(Context? context)
+            : base(context)
+        {
+            InitializeImpl();
+            Initialize();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimeSlider"/> class.
+        /// </summary>
+        /// <param name="context">The Context the view is running in, through which it can access resources, themes, etc.</param>
+        /// <param name="attr">The attributes of the AXML element declaring the view.</param>
+        public TimeSlider(Context? context, IAttributeSet? attr)
+            : base(context, attr)
+        {
+            InitializeImpl();
+            Initialize();
+        }
+
+        private void InitializeImpl()
+        {
+            if (DesignTime.IsDesignMode)
+            {
+                // Add placeholder text
+                SetBackgroundColor(Color.LightGray);
+                var designTimePlaceholderText = new TextView(Context)
+                {
+                    Text = "Time Slider",
+                    TextSize = 16,
+                    Id = 123456789,
+                };
+                designTimePlaceholderText.SetTextColor(Color.Black);
+                AddView(designTimePlaceholderText);
+
+                // Center text by constraining it to the edges of the parent view
+                var constraintSet = new ConstraintSet();
+                constraintSet.Clone(this);
+                constraintSet.Connect(designTimePlaceholderText.Id, ConstraintSet.Start, ConstraintSet.ParentId, ConstraintSet.Start);
+                constraintSet.Connect(designTimePlaceholderText.Id, ConstraintSet.End, ConstraintSet.ParentId, ConstraintSet.End);
+                constraintSet.Connect(designTimePlaceholderText.Id, ConstraintSet.Top, ConstraintSet.ParentId, ConstraintSet.Top, 15);
+                constraintSet.Connect(designTimePlaceholderText.Id, ConstraintSet.Bottom, ConstraintSet.ParentId, ConstraintSet.Bottom, 15);
+                constraintSet.ApplyTo(this);
+
+#pragma warning disable CS8774 // Member must have a non-null value when exiting.
+                return;
+#pragma warning restore CS8774 // Member must have a non-null value when exiting.
+            }
+
+            var inflater = LayoutInflater.FromContext(Context!);
+            inflater?.Inflate(Resource.Layout.TimeSlider, this, true);
+
+            SliderTrack = FindViewById<View>(Resource.Id.SliderTrack);
+            SliderTrackOutline = FindViewById<View>(Resource.Id.SliderTrackOutline);
+            FullExtentStartTimeLabel = FindViewById<TextView>(Resource.Id.FullExtentStartTimeLabel);
+            FullExtentEndTimeLabel = FindViewById<TextView>(Resource.Id.FullExtentEndTimeLabel);
+            MinimumThumb = FindViewById<View>(Resource.Id.MinThumb);
+            MaximumThumb = FindViewById<View>(Resource.Id.MaxThumb);
+            MinimumThumbLabel = FindViewById<TextView>(Resource.Id.CurrentExtentStartTimeLabel);
+            MaximumThumbLabel = FindViewById<TextView>(Resource.Id.CurrentExtentEndTimeLabel);
+            PinnedMinimumThumb = FindViewById<View>(Resource.Id.PinnedMinThumb);
+            PinnedMaximumThumb = FindViewById<View>(Resource.Id.PinnedMaxThumb);
+            HorizontalTrackThumb = FindViewById<View>(Resource.Id.CurrentExtentFill);
+            Tickmarks = FindViewById<Tickbar>(Resource.Id.Tickmarks);
+            PlayPauseButton = FindViewById<ToggleButton>(Resource.Id.PlayPauseButton);
+            PlayButtonOutline = FindViewById<View>(Resource.Id.PlayButtonOutline);
+            PauseButtonOutline = FindViewById<View>(Resource.Id.PauseButtonOutline);
+            NextButton = FindViewById<Button>(Resource.Id.NextButton);
+            PreviousButton = FindViewById<Button>(Resource.Id.PreviousButton);
+            NextButtonOutline = FindViewById<View>(Resource.Id.NextButtonOutline);
+            PreviousButtonOutline = FindViewById<View>(Resource.Id.PreviousButtonOutline);
+            _startTimeTickmark = FindViewById<View>(Resource.Id.FullExtentStartTimeTickmark);
+            _endTimeTickmark = FindViewById<View>(Resource.Id.FullExtentEndTimeTickmark);
+
+            PositionTickmarks();
+            ApplyLabelMode(LabelMode);
+
+            if (PlayPauseButton != null)
+            {
+                PlayPauseButton.CheckedChange += (o, e) =>
+                {
+                    IsPlaying = PlayPauseButton.Checked;
+                    if (PlayButtonOutline != null)
+                    {
+                        PlayButtonOutline.Visibility = IsPlaying ? ViewStates.Gone : ViewStates.Visible;
+                    }
+
+                    if (PauseButtonOutline != null)
+                    {
+                        PauseButtonOutline.Visibility = IsPlaying ? ViewStates.Visible : ViewStates.Gone;
+                    }
+                };
+            }
+
+            if (NextButton != null)
+            {
+                NextButton.Click += (o, e) => OnNextButtonClick();
+            }
+
+            if (PreviousButton != null)
+            {
+                PreviousButton.Click += (o, e) => OnPreviousButtonClick();
+            }
+
+            SetOnTouchListener(this);
+        }
+
+        private void InvalidateMeasureAndArrange()
+        {
+            if (CurrentValidExtent != null)
+            {
+                UpdateTrackLayout(CurrentValidExtent);
+            }
+        }
+
+        private Size GetDesiredSize(View view)
+        {
+            view.Measure((int)MeasureSpecMode.Unspecified, (int)MeasureSpecMode.Unspecified);
+            return new Size((int)view.MeasuredWidth, (int)view.MeasuredHeight);
+        }
+
+        /// <inheritdoc />
+        public bool OnTouch(View? v, MotionEvent? e)
+        {
+            if (e is null || MinimumThumb is null || MaximumThumb is null)
+            {
+                return false;
+            }
+
+            // Get x/y coordinates of touch location
+            var touchX = e.RawX;
+            var touchY = e.RawY;
+
+            var isTouchHandled = _isMinThumbFocused || _isMaxThumbFocused;
+            var minTargetSize = TypedValue.ApplyDimension(ComplexUnitType.Dip, 44f, ViewExtensions.GetDisplayMetrics(Context));
+            var minThumbBounds = GetBounds(MinimumThumb, minTargetSize, minTargetSize);
+            var maxThumbBounds = GetBounds(MaximumThumb, minTargetSize, minTargetSize);
+            switch (e.Action)
+            {
+                case MotionEventActions.Down:
+                    _isMinThumbFocused = minThumbBounds.Contains(touchX, touchY);
+                    _isMaxThumbFocused = maxThumbBounds.Contains(touchX, touchY);
+
+                    if (_isMinThumbFocused && !IsStartTimePinned)
+                    {
+                        _lastX = touchX - minThumbBounds.Left;
+                        isTouchHandled = true;
+                    }
+
+                    if (_isMaxThumbFocused && !IsEndTimePinned)
+                    {
+                        _lastX = touchX - maxThumbBounds.Left;
+                        isTouchHandled = true;
+                    }
+
+                    break;
+                case MotionEventActions.Move:
+                    if (!_isMinThumbFocused && !_isMaxThumbFocused && !(_isMinThumbFocused && IsStartTimePinned) && !(_isMaxThumbFocused && IsEndTimePinned))
+                    {
+                        return isTouchHandled;
+                    }
+
+                    View? trackedThumb = null;
+                    if (_isMinThumbFocused && _isMaxThumbFocused)
+                    {
+                        var maxThumbTranslateX = touchX - maxThumbBounds.Left - _lastX;
+
+                        // Gesture was within both min and max thumb, so let the direction of the gesture determine which thumb should be dragged
+                        if (maxThumbTranslateX < 0)
+                        {
+                            // Gesture is moving thumb toward the min, so put focus on min thumb
+                            trackedThumb = MinimumThumb;
+                            _isMaxThumbFocused = false;
+                        }
+                        else
+                        {
+                            // Gesture is moving thumb toward the max, so put focus on max thumb
+                            trackedThumb = MaximumThumb;
+                            _isMinThumbFocused = false;
+                        }
+                    }
+                    else if (_isMinThumbFocused)
+                    {
+                        trackedThumb = MinimumThumb;
+                    }
+                    else if (_isMaxThumbFocused)
+                    {
+                        trackedThumb = MaximumThumb;
+                    }
+
+                    var currentThumbX = touchX - trackedThumb?.Left ?? 0;
+                    var translateX = currentThumbX - _lastX;
+
+                    if (_isMinThumbFocused)
+                    {
+                        OnMinimumThumbDrag(translateX);
+                    }
+
+                    if (_isMaxThumbFocused)
+                    {
+                        OnMaximumThumbDrag(translateX);
+                    }
+
+                    break;
+                case MotionEventActions.Up:
+                    _isMinThumbFocused = false;
+                    _isMaxThumbFocused = false;
+                    OnDragCompleted();
+                    break;
+            }
+
+            return isTouchHandled;
+        }
+
+        private RectF GetBounds(View view, float minWidth, float minHeight)
+        {
+            var xy = new int[2];
+            view.GetLocationOnScreen(xy);
+            var left = xy[0];
+            var right = left + view.GetActualWidth();
+            var top = xy[1];
+            var bottom = top + view.GetActualHeight();
+
+            var width = right - left;
+            if (width < minWidth)
+            {
+                var expansionFactor = (int)((minWidth - width) / 2);
+                left -= expansionFactor;
+                right += expansionFactor;
+            }
+
+            var height = bottom - top;
+            if (height < minHeight)
+            {
+                var expansionFactor = (int)((minHeight - height) / 2);
+                top -= expansionFactor;
+                bottom += expansionFactor;
+            }
+
+            return new RectF((float)left, (float)top, (float)right, (float)bottom);
+        }
+
+        /// <inheritdoc />
+        protected async override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
+        {
+            base.OnMeasure(widthMeasureSpec, heightMeasureSpec);
+
+            await _measureThrottler.ThrottleDelay();
+            if (Tickmarks != null && SliderTrack != null)
+            {
+                Tickmarks.TickInset = (float)(this.GetActualWidth() - SliderTrack.GetActualWidth()) / 2;
+            }
+        }
+    }
+}
---
+++ src/Toolkit/Toolkit.Maui/Platform/iOS/Tickbar.cs
@@ -0,0 +1,276 @@
+﻿// /*******************************************************************************
+//  * Copyright 2012-2018 Esri
+//  *
+//  *  Licensed under the Apache License, Version 2.0 (the "License");
+//  *  you may not use this file except in compliance with the License.
+//  *  You may obtain a copy of the License at
+//  *
+//  *  http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  *   Unless required by applicable law or agreed to in writing, software
+//  *   distributed under the License is distributed on an "AS IS" BASIS,
+//  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  *   See the License for the specific language governing permissions and
+//  *   limitations under the License.
+//  ******************************************************************************/
+
+#if __IOS__
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using CoreGraphics;
+using Esri.ArcGISRuntime.Toolkit.Internal;
+using Esri.ArcGISRuntime.Toolkit.UI;
+using Microsoft.Maui.Controls.Compatibility.Platform.iOS;
+using UIKit;
+
+namespace Esri.ArcGISRuntime.Toolkit.Primitives
+{
+    public partial class Tickbar : UIView
+    {
+        private void Initialize()
+        {
+        }
+
+        private CGSize _majorTickSize = new CGSize(1, 10);
+
+        /// <summary>
+        /// Gets or sets the size of major tickmarks.
+        /// </summary>
+        public CGSize MajorTickSize
+        {
+            get => _majorTickSize;
+            set
+            {
+                if (value.Width == _majorTickSize.Width && value.Height == _majorTickSize.Height)
+                {
+                    return;
+                }
+
+                _majorTickSize = value;
+                foreach (RectangleView tick in _majorTickmarks)
+                {
+                    tick.Width = _majorTickSize.Width;
+                    tick.Height = _majorTickSize.Height;
+                }
+
+                InvalidateMeasureAndArrange();
+            }
+        }
+
+        private CGSize _minorTickSize = new CGSize(1, 5);
+
+        /// <summary>
+        /// Gets or sets the size of minor tickmarks.
+        /// </summary>
+        public CGSize MinorTickSize
+        {
+            get => _minorTickSize;
+            set
+            {
+                if (value.Width == _minorTickSize.Width && value.Height == _minorTickSize.Height)
+                {
+                    return;
+                }
+
+                _minorTickSize = value;
+                foreach (RectangleView tick in _minorTickmarks)
+                {
+                    tick.Width = _minorTickSize.Width;
+                    tick.Height = _minorTickSize.Height;
+                }
+
+                InvalidateMeasureAndArrange();
+            }
+        }
+
+        private double _labelOffset = 4;
+
+        /// <summary>
+        /// Gets or sets the spacing between ticks and tick labels.
+        /// </summary>
+        public double LabelOffset
+        {
+            get => _labelOffset;
+            set
+            {
+                if (_labelOffset == value)
+                {
+                    return;
+                }
+
+                _labelOffset = value;
+                InvalidateMeasureAndArrange();
+            }
+        }
+
+        /// <summary>
+        /// Adds a tick to the bar's visual tree.
+        /// </summary>
+        /// <param name="position">The position to place the tick at along the tick bar.</param>
+        /// <param name="dataSource">The data to pass to the tick's template.</param>
+        private void AddTickmark(double position, object? dataSource)
+        {
+            // Create both a minor and major tick mark at the specified position.  Layout logic will determine which
+            // one to actually show at the position.
+
+            // Create a minor tickmark
+            var tick = new RectangleView()
+            {
+                BackgroundColor = TickFill,
+                Width = MinorTickSize.Width,
+                Height = MinorTickSize.Height,
+                BorderWidth = 0,
+            };
+            SetIsMajorTickmark(tick, false);
+            SetPosition(tick, position);
+
+            AddSubview(tick);
+            _minorTickmarks.Add(tick);
+
+            // Create a major tickmark
+            tick = new RectangleView()
+            {
+                BackgroundColor = TickFill,
+                Width = MajorTickSize.Width,
+                Height = MajorTickSize.Height,
+                BorderWidth = 0,
+            };
+
+            if (dataSource is DateTimeOffset dateTime)
+            {
+                var majorTickContainer = new UIView();
+
+                // Create label for major tickmark
+                var timeStepIntervalDateFormat = string.IsNullOrEmpty(TickLabelFormat)
+                    ? _defaultTickLabelFormat : TickLabelFormat;
+                var label = new UILabel()
+                {
+                    Text = dateTime.ToString(timeStepIntervalDateFormat),
+                    Font = UIFont.SystemFontOfSize(11),
+                    TextColor = TickLabelColor,
+                    Tag = (nint)dateTime.ToUnixTimeMilliseconds(),
+                };
+
+                // Calculate positions of the tickmark and label
+                var labelSize = label.SizeThatFits(Frame.Size);
+                var tickLeft = (labelSize.Width - tick.Width) / 2;
+                tick.Frame = new CGRect(tickLeft, 0, tick.Width, tick.Height);
+                label.Frame = new CGRect(0, tick.Height + LabelOffset, labelSize.Width, labelSize.Height);
+
+                majorTickContainer.AddSubviews(tick, label);
+                majorTickContainer.Frame = new CGRect(0, 0, labelSize.Width, label.Frame.Bottom);
+
+                // Flag the tick as a major tickmark and set it's proportional position along the tick bar
+                SetIsMajorTickmark(majorTickContainer, true);
+                SetPosition(majorTickContainer, position);
+
+                AddSubview(majorTickContainer);
+                _majorTickmarks.Add(majorTickContainer);
+            }
+            else
+            {
+                SetIsMajorTickmark(tick, true);
+                SetPosition(tick, position);
+
+                AddSubview(tick);
+                _majorTickmarks.Add(tick);
+            }
+        }
+
+        private void ApplyTickLabelFormat(UIView tick, string? tickLabelFormat)
+        {
+            // Retrieve the label from the container holding the major tick rectangle and label
+            if (tick.Subviews.Length > 1 && tick.Subviews[1] is UILabel label)
+            {
+                // Apply the specified format to the tick's date and update the label
+                var labelFormat = string.IsNullOrEmpty(tickLabelFormat) ? _defaultTickLabelFormat : tickLabelFormat;
+                var labelDate = DateTimeOffset.FromUnixTimeMilliseconds(label.Tag);
+                label.Text = labelDate.ToString(labelFormat);
+            }
+        }
+
+        private void ApplyTickLabelColor(UIView tick, UIColor color)
+        {
+            if (tick.Subviews.Length > 1 && tick.Subviews[1] is UILabel label)
+            {
+                label.TextColor = color;
+            }
+        }
+
+        private UIView[] Children => Subviews;
+
+        private nfloat Width => Frame.Width;
+
+        private nfloat Height => Frame.Height;
+
+        private void SetIsMajorTickmark(UIView view, bool isMajorTickmark)
+        {
+            UpdatePositionAndIsMajorTickmark(view, GetPosition(view), isMajorTickmark);
+        }
+
+        private bool GetIsMajorTickmark(UIView view)
+        {
+            return view.Tag % 10 == 0 ? false : true;
+        }
+
+        private double GetPosition(UIView view)
+        {
+            // Remove last digit as that stores whether the tickmark is major or minor
+            var positionDigits = Math.Truncate((double)view.Tag / 10);
+
+            // Convert remaining digits to decimal value between 0 and 1
+            return positionDigits / 10000000;
+        }
+
+        private void SetPosition(UIView view, double position)
+        {
+            UpdatePositionAndIsMajorTickmark(view, position, GetIsMajorTickmark(view));
+        }
+
+        private void UpdatePositionAndIsMajorTickmark(UIView view, double position, bool isMajorTickmark)
+        {
+            // Use the view's tag property to store both the tick's proportional position along the tick bar and whether
+            // or not the tick is a major tickmark.  Use the first 9 digits to store the position and the last digit to
+            // store the flag.
+            var storedPosition = Math.Truncate(position * 100000000);
+            storedPosition -= storedPosition % 10;
+            var tickmarkFlagInt = isMajorTickmark ? 1 : 0;
+            var positionAndTickmarkFlag = storedPosition + tickmarkFlagInt;
+            view.Tag = (nint)positionAndTickmarkFlag;
+        }
+
+        private CGSize GetDesiredSize(UIView view) => view.SizeThatFits(Frame.Size);
+
+        private int ChildCount => Subviews.Length;
+
+        private void InvalidateMeasureAndArrange()
+        {
+            // Re-layout major tickmarks and labels to accommodate possible change in label lengths
+            foreach (var tickContainer in _majorTickmarks)
+            {
+                // Get tick rectangle and label for current tick
+                var tick = tickContainer.Subviews.OfType<RectangleView>().FirstOrDefault();
+                var label = tickContainer.Subviews.OfType<UILabel>().FirstOrDefault();
+                if (tick == null || label == null)
+                {
+                    continue;
+                }
+
+                // Get size of the tick label and calculate the frame for the tick, label, and container
+                var labelSize = label.SizeThatFits(Frame.Size);
+                var tickLeft = (labelSize.Width - tick.Width) / 2;
+                tick.Frame = new CGRect(tickLeft, 0, tick.Width, tick.Height);
+                label.Frame = new CGRect(0, tick.Height + LabelOffset, labelSize.Width, labelSize.Height);
+                tickContainer.Frame = new CGRect(0, 0, labelSize.Width, label.Frame.Bottom);
+            }
+
+            OnArrange(Frame.Size);
+        }
+    }
+}
+
+#endif
