28298ca7867caa79dfe7c021368ac37779fb7cce
Nathan Castle <ncastle@esri.com>
2022-09-10 15:12:08 -0700

    Fix several issues with BasemapGallery

--- src/Toolkit/Toolkit.Maui/BasemapGallery/ByteArrayToImageSourceConverter.cs
+++ src/Toolkit/Toolkit.Maui/BasemapGallery/ByteArrayToImageSourceConverter.cs
@@ -30,7 +30,7 @@ namespace Esri.ArcGISRuntime.Toolkit.Maui
                 return ImageSource.FromStream(() => new MemoryStream(rawBuffer));
             }
 
-            return null;
+            return ImageSource.FromResource("Esri.ArcGISRuntime.Toolkit.Maui.Assets.BasemapLight.png", typeof(BasemapGallery).Assembly); ;
         }
 
         /// <inheritdoc/>
