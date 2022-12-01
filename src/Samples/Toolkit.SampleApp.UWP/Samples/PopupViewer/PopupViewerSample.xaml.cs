b0e8fd2f658084cf4fb3ba682782f3a1d5cfc9f4
Nathan Castle <ncastle@esri.com>
2022-12-01 13:52:20 -0800

    WinUI+UWP - Popup sample - fix button issue
    The use of the image button has caused issues for this sample several times over the years. It was past due to simplify the functional test to focus on the important part, which is showing the popup viewer.

--- src/Samples/Toolkit.SampleApp.UWP/Samples/PopupViewer/PopupViewerSample.xaml.cs
+++ src/Samples/Toolkit.SampleApp.UWP/Samples/PopupViewer/PopupViewerSample.xaml.cs
@@ -26,22 +26,6 @@ namespace Esri.ArcGISRuntime.Toolkit.SampleApp.Samples.PopupViewer
         // Webmap configured with Popup
         public Map Map { get; } = new Map(new Uri("https://www.arcgis.com/home/item.html?id=d4fe39d300c24672b1821fa8450b6ae2"));
 
-        private RuntimeImage _infoIcon = null;
-        // Used in Callout to see feature details in PopupViewer
-        private RuntimeImage InfoIcon
-        {
-            get
-            {
-                if (_infoIcon == null)
-                {
-                    var fileName = RequestedTheme == ElementTheme.Dark ? "info_light.png" : "info_dark.png";
-                    _infoIcon = new RuntimeImage(new Uri($"ms-appx:///Samples/PopupViewer/{fileName}"));
-                }
-                return _infoIcon;
-            }
-        }
-
-
         private async void mapView_GeoViewTapped(object sender, GeoViewInputEventArgs e)
         {
             Exception error = null;
@@ -55,15 +39,8 @@ namespace Esri.ArcGISRuntime.Toolkit.SampleApp.Samples.PopupViewer
                 // Displays callout and updates visibility of PopupViewer
                 if (popup != null)
                 {
-                    var callout = new CalloutDefinition(popup.GeoElement);
-                    callout.Tag = popup;
-                    callout.ButtonImage = InfoIcon;
-                    callout.OnButtonClick = new Action<object>((s) =>
-                    {
-                        popupViewer.Visibility = Visibility.Visible;
-                        popupViewer.PopupManager = new PopupManager(s as Mapping.Popups.Popup);
-                    });
-                    mapView.ShowCalloutForGeoElement(popup.GeoElement, e.Position, callout);
+                    popupViewer.Visibility = Visibility.Visible;
+                    popupViewer.PopupManager = new PopupManager(popup);
                 }
                 else
                 {
@@ -76,7 +53,7 @@ namespace Esri.ArcGISRuntime.Toolkit.SampleApp.Samples.PopupViewer
                 error = ex;
             }
             if (error != null)
-                await new MessageDialog(error.Message, error.GetType().Name).ShowAsync();
+                System.Diagnostics.Debug.WriteLine(error);
         }
 
         private Mapping.Popups.Popup GetPopup(IdentifyLayerResult result)
