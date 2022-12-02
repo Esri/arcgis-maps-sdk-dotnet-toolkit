fef50f54b38f7dc72c3ed11a2b53e5be4b20fd63
Nathan Castle <ncastle@esri.com>
2022-12-02 15:47:02 -0800

    Fix dark mode on MAUI search

--- src/Toolkit/Toolkit.Maui/SearchView/SearchView.Appearance.cs
+++ src/Toolkit/Toolkit.Maui/SearchView/SearchView.Appearance.cs
@@ -32,6 +32,10 @@ public partial class SearchView : TemplatedView, INotifyPropertyChanged
     private Grid? PART_ResultContainer;
     private Grid? PART_RepeatButtonContainer;
 #pragma warning restore SA1306, SA1310, SX1309 // Field names should begin with lower-case letter
+    private const string FOREGROUND_LIGHT = "#151515";
+    private const string FOREGROUND_DARK = "#FFF";
+    private const string BACKGROUND_LIGHT = "#FFF";
+    private const string BACKGROUND_DARK = "#2B2B2B";
 
     private static readonly DataTemplate DefaultResultTemplate;
     private static readonly DataTemplate DefaultSuggestionTemplate;
@@ -49,7 +53,7 @@ public partial class SearchView : TemplatedView, INotifyPropertyChanged
         DefaultSuggestionGroupHeaderTemplate = new DataTemplate(() =>
         {
             Grid containingGrid = new Grid();
-            containingGrid.BackgroundColor = Color.FromArgb("#4e4e4e");
+            containingGrid.SetAppThemeColor(Grid.BackgroundColorProperty, Color.FromArgb("#4e4e4e"), Color.FromArgb("#151515"));
 
             Label textLabel = new Label();
             textLabel.SetBinding(Label.TextProperty, "Key.DisplayName");
@@ -63,12 +67,13 @@ public partial class SearchView : TemplatedView, INotifyPropertyChanged
         DefaultSuggestionTemplate = new DataTemplate(() =>
         {
             Grid containingGrid = new Grid();
-
+            containingGrid.SetAppThemeColor(Grid.BackgroundColorProperty, Color.FromArgb(BACKGROUND_LIGHT), Color.FromArgb(BACKGROUND_DARK));
             containingGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
             containingGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
             containingGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
 
             Grid textStack = new Grid();
+            textStack.BackgroundColor = Colors.Transparent;
             textStack.VerticalOptions = LayoutOptions.Center;
             textStack.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             textStack.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
@@ -84,14 +89,14 @@ public partial class SearchView : TemplatedView, INotifyPropertyChanged
             titleLabel.SetBinding(Label.TextProperty, nameof(SearchSuggestion.DisplayTitle));
             titleLabel.VerticalOptions = LayoutOptions.End;
             titleLabel.VerticalTextAlignment = TextAlignment.End;
-            titleLabel.TextColor = Colors.Black;
+            titleLabel.SetAppThemeColor(Label.TextColorProperty, Color.FromArgb(FOREGROUND_LIGHT), Color.FromArgb(FOREGROUND_DARK));
 
             Label subtitleLabel = new Label();
             subtitleLabel.SetBinding(Label.TextProperty, nameof(SearchSuggestion.DisplaySubtitle));
             subtitleLabel.SetBinding(Label.IsVisibleProperty, nameof(SearchSuggestion.DisplaySubtitle), converter: EmptyStringConverter);
             subtitleLabel.VerticalOptions = LayoutOptions.Start;
             subtitleLabel.VerticalTextAlignment = TextAlignment.Start;
-            subtitleLabel.TextColor = Colors.Black;
+            subtitleLabel.SetAppThemeColor(Label.TextColorProperty, Color.FromArgb(FOREGROUND_LIGHT), Color.FromArgb(FOREGROUND_DARK));
 
             textStack.Children.Add(titleLabel);
             textStack.Children.Add(subtitleLabel);
@@ -110,12 +115,14 @@ public partial class SearchView : TemplatedView, INotifyPropertyChanged
         {
             Grid containingGrid = new Grid();
             containingGrid.Padding = new Thickness(2, 4, 2, 4);
+            containingGrid.SetAppThemeColor(Grid.BackgroundColorProperty, Color.FromArgb(BACKGROUND_LIGHT), Color.FromArgb(BACKGROUND_DARK));
 
             containingGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
             containingGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
             containingGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
 
             Grid textStack = new Grid();
+            textStack.BackgroundColor = Colors.Transparent;
             textStack.VerticalOptions = LayoutOptions.Center;
             textStack.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             textStack.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
@@ -132,14 +139,14 @@ public partial class SearchView : TemplatedView, INotifyPropertyChanged
             titleLabel.FontAttributes = FontAttributes.Bold;
             titleLabel.VerticalOptions = LayoutOptions.End;
             titleLabel.VerticalTextAlignment = TextAlignment.End;
-            titleLabel.TextColor = Colors.Black;
+            titleLabel.SetAppThemeColor(Label.TextColorProperty, Color.FromArgb(FOREGROUND_LIGHT), Color.FromArgb(FOREGROUND_DARK));
 
             Label subtitleLabel = new Label();
             subtitleLabel.SetBinding(Label.TextProperty, nameof(SearchResult.DisplaySubtitle));
             subtitleLabel.SetBinding(Label.IsVisibleProperty, nameof(SearchResult.DisplaySubtitle), converter: EmptyStringConverter);
-            subtitleLabel.TextColor = Colors.Black;
             subtitleLabel.VerticalOptions = LayoutOptions.Start;
             subtitleLabel.VerticalTextAlignment = TextAlignment.Start;
+            subtitleLabel.SetAppThemeColor(Label.TextColorProperty, Color.FromArgb(FOREGROUND_LIGHT), Color.FromArgb(FOREGROUND_DARK));
 
             textStack.Children.Add(titleLabel);
             textStack.Children.Add(subtitleLabel);
@@ -158,7 +165,19 @@ public partial class SearchView : TemplatedView, INotifyPropertyChanged
         string template =
 $@"<ControlTemplate xmlns=""http://schemas.microsoft.com/dotnet/2021/maui"" xmlns:x=""http://schemas.microsoft.com/winfx/2009/xaml"" 
 xmlns:esriTK=""clr-namespace:Esri.ArcGISRuntime.Toolkit.Maui"">
-<Grid RowSpacing=""0"" ColumnSpacing=""0"" >
+<Grid RowSpacing=""0"" ColumnSpacing=""0"">
+<Grid.Resources>
+        <Style TargetType=""Grid"">
+            <Setter Property=""Background"" Value=""{{AppThemeBinding Dark={BACKGROUND_DARK},Light={BACKGROUND_LIGHT}}}"" />
+        </Style>
+        <Style TargetType=""CollectionView"">
+            <Setter Property=""Background"" Value=""{{AppThemeBinding Dark={BACKGROUND_DARK},Light={BACKGROUND_LIGHT}}}"" />
+        </Style>
+        <Style TargetType=""Entry"">
+            <Setter Property=""Background"" Value=""{{AppThemeBinding Dark={BACKGROUND_DARK},Light={BACKGROUND_LIGHT}}}"" />
+        </Style>
+
+</Grid.Resources>
     <Grid.ColumnDefinitions>
     <ColumnDefinition Width=""Auto"" />
     <ColumnDefinition Width=""*"" />
@@ -169,17 +188,17 @@ xmlns:esriTK=""clr-namespace:Esri.ArcGISRuntime.Toolkit.Maui"">
     <RowDefinition Height=""Auto"" />
     <RowDefinition Height=""Auto"" />
     </Grid.RowDefinitions>
-    <Grid Grid.Row=""0"" Grid.ColumnSpan=""3"" BackgroundColor=""White"" />
+    <Grid Grid.Row=""0"" Grid.ColumnSpan=""3""/>
     <ImageButton x:Name=""{nameof(PART_SourceSelectButton)}"" Grid.Column=""0"" WidthRequest=""32"" HeightRequest=""32"" Padding=""4"" BackgroundColor=""Transparent"" Margin=""0"" />
-    <Entry x:Name=""{nameof(PART_Entry)}"" Grid.Column=""1"" Grid.Row=""0"" BackgroundColor=""White"" TextColor=""Black"" />
+    <Entry x:Name=""{nameof(PART_Entry)}"" Grid.Column=""1"" Grid.Row=""0"" TextColor=""{{AppThemeBinding Light={FOREGROUND_LIGHT}, Dark={FOREGROUND_DARK}}}"" />
     <ImageButton x:Name=""{nameof(PART_CancelButton)}"" Grid.Column=""1"" HorizontalOptions=""End"" WidthRequest=""32"" HeightRequest=""32"" Padding=""4"" BackgroundColor=""Transparent"" />
     <ImageButton x:Name=""{nameof(PART_SearchButton)}"" Grid.Column=""2"" WidthRequest=""32"" HeightRequest=""32"" Padding=""4"" BackgroundColor=""Transparent"" />
-    <CollectionView x:Name=""{nameof(PART_SuggestionsView)}"" SelectionMode=""Single"" Grid.Column=""0"" Grid.ColumnSpan=""3"" Grid.Row=""1"" Grid.RowSpan=""2"" BackgroundColor=""White"" HeightRequest=""175"" />
-    <CollectionView x:Name=""{nameof(PART_ResultView)}"" SelectionMode=""Single"" Grid.Column=""0"" Grid.ColumnSpan=""3"" Grid.Row=""1"" Grid.RowSpan=""1"" BackgroundColor=""White"" HeightRequest=""200"" />
-    <CollectionView x:Name=""{nameof(PART_SourcesView)}"" SelectionMode=""Single"" Grid.Column=""0"" Grid.ColumnSpan=""3"" Grid.Row=""1"" BackgroundColor=""White"" HeightRequest=""150"" />
-    <Grid x:Name=""{nameof(PART_ResultContainer)}"" BackgroundColor=""White"" Grid.ColumnSpan=""3"" Grid.Row=""1"" Padding=""8""><Label x:Name=""{nameof(PART_ResultLabel)}"" HorizontalOptions=""Center"" VerticalOptions=""Center"" FontAttributes=""Bold"" /></Grid>
-    <Grid x:Name=""{nameof(PART_RepeatButtonContainer)}"" BackgroundColor=""White"" Grid.Column=""0"" Grid.ColumnSpan=""3""  Grid.Row=""2"">
-        <Button x:Name=""{nameof(PART_RepeatButton)}"" BackgroundColor=""#007AC2"" TextColor=""White"" CornerRadius=""0"" />
+    <CollectionView x:Name=""{nameof(PART_SuggestionsView)}"" SelectionMode=""Single"" Grid.Column=""0"" Grid.ColumnSpan=""3"" Grid.Row=""1"" Grid.RowSpan=""2""  HeightRequest=""175"" />
+    <CollectionView x:Name=""{nameof(PART_ResultView)}"" SelectionMode=""Single"" Grid.Column=""0"" Grid.ColumnSpan=""3"" Grid.Row=""1"" Grid.RowSpan=""1"" HeightRequest=""200"" />
+    <CollectionView x:Name=""{nameof(PART_SourcesView)}"" SelectionMode=""Single"" Grid.Column=""0"" Grid.ColumnSpan=""3"" Grid.Row=""1"" HeightRequest=""150"" />
+    <Grid x:Name=""{nameof(PART_ResultContainer)}"" Grid.ColumnSpan=""3"" Grid.Row=""1"" Padding=""8""><Label x:Name=""{nameof(PART_ResultLabel)}"" HorizontalOptions=""Center"" VerticalOptions=""Center"" FontAttributes=""Bold"" /></Grid>
+    <Grid x:Name=""{nameof(PART_RepeatButtonContainer)}"" Grid.Column=""0"" Grid.ColumnSpan=""3""  Grid.Row=""2"">
+        <Button x:Name=""{nameof(PART_RepeatButton)}"" BackgroundColor=""{{AppThemeBinding Light=#007AC2, Dark=#00619B}}"" TextColor=""White"" CornerRadius=""0"" />
     </Grid>
 </Grid>
 </ControlTemplate>";
