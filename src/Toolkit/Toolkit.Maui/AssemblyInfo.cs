50b5f722a537f07e7acfdb644b09ac80cb351650
Morten Nielsen <mort5161@esri.com>
2023-03-30 09:11:25 -0700

    Fix min-version support in MAUI

--- src/Toolkit/Toolkit.Maui/AssemblyInfo.cs
+++ src/Toolkit/Toolkit.Maui/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿#if !__IOS__ && !WINDOWS && !__ANDROID__ && !__MACCATALYST__
 // Make sure net6.0 non-os target clearly states which platforms are supported by the toolkit
-[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows10.0.19041")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("windows10.0.17763")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatform("android26.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatform("ios14.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatform("maccatalyst14.0")]
