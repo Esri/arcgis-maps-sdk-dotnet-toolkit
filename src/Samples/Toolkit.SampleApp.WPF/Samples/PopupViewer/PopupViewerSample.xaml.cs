841802b797f79ce3ede0a87d586add2089e2dbdf
Matvei Stefarov <mstefarov@esri.com>
2023-04-12 11:24:16 -0700

    PopupViewer sample: check all results for popups before building new one

--- src/Samples/Toolkit.SampleApp.WPF/Samples/PopupViewer/PopupViewerSample.xaml.cs
+++ src/Samples/Toolkit.SampleApp.WPF/Samples/PopupViewer/PopupViewerSample.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Mapping.Popups;
 using Esri.ArcGISRuntime.UI;
 using Esri.ArcGISRuntime.UI.Controls;
@@ -31,7 +31,7 @@ namespace Esri.ArcGISRuntime.Toolkit.Samples.PopupViewer
                 var result = await mapView.IdentifyLayersAsync(e.Position, 3, false);
 
                 // Retrieves or builds Popup from IdentifyLayerResult
-                var popup = GetPopup(result);
+                var popup = GetPopup(result) ?? BuildPopupFromGeoElement(result);
                 if (popup != null)
                 {
                     PopupBackground.Visibility = Visibility.Visible;
@@ -47,59 +47,67 @@ namespace Esri.ArcGISRuntime.Toolkit.Samples.PopupViewer
         private Popup GetPopup(IdentifyLayerResult result)
         {
             if (result == null)
-            {
                 return null;
-            }
-
             var popup = result.Popups.FirstOrDefault();
             if (popup != null)
             {
+                if (popup.GeoElement is DynamicEntityObservation deo)
+                {
+                    return new Popup(deo.GetDynamicEntity(), popup.PopupDefinition);
+                }
                 return popup;
             }
 
-            var geoElement = result.GeoElements.FirstOrDefault();
-            if (geoElement != null)
-            {
-                if (result.LayerContent is IPopupSource)
-                {
-                    var popupDefinition = ((IPopupSource)result.LayerContent).PopupDefinition;
-                    if (popupDefinition != null)
-                    {
-                        return new Popup(geoElement, popupDefinition);
-                    }
-                }
-
-                return Popup.FromGeoElement(geoElement);
-            }
-
-            return null;
+            return GetPopup(result.SublayerResults);
         }
 
         private Popup GetPopup(IEnumerable<IdentifyLayerResult> results)
         {
             if (results == null)
-            {
                 return null;
-            }
             foreach (var result in results)
             {
                 var popup = GetPopup(result);
                 if (popup != null)
-                {
                     return popup;
-                }
-
-                foreach (var subResult in result.SublayerResults)
-                {
-                    popup = GetPopup(subResult);
-                    if (popup != null)
-                    {
-                        return popup;
-                    }
-                }
             }
 
-            return null; 
+            return null;
+        }
+
+        private Popup BuildPopupFromGeoElement(IdentifyLayerResult result)
+        {
+            if (result == null)
+                return null;
+            var geoElement = result.GeoElements.FirstOrDefault();
+            if (geoElement != null)
+            {
+                if (geoElement is DynamicEntityObservation obs)
+                    geoElement = obs.GetDynamicEntity();
+                if (result.LayerContent is IPopupSource source)
+                {
+                    var popupDefinition = source.PopupDefinition;
+                    if (popupDefinition != null)
+                    {
+                        return new Popup(geoElement, popupDefinition);
+                    }
+                }
+                return Popup.FromGeoElement(geoElement);
+            }
+            return BuildPopupFromGeoElement(result.SublayerResults);
+        }
+
+        private Popup BuildPopupFromGeoElement(IEnumerable<IdentifyLayerResult> results)
+        {
+            if (results == null)
+                return null;
+            foreach (var result in results)
+            {
+                var popup = BuildPopupFromGeoElement(result);
+                if (popup != null)
+                    return popup;
+            }
+            return null;
         }
 
         private void PopupBackground_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
