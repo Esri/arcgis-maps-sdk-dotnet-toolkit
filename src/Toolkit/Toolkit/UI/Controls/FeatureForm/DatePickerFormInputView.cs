243e4cfbd688636264c0e32fa96e164b14f8c3fc
Morten Nielsen <mort5161@esri.com>
2024-01-31 12:27:39 -0800

    Add date picker

---
+++ src/Toolkit/Toolkit/UI/Controls/FeatureForm/DatePickerFormInputView.cs
@@ -0,0 +1,114 @@
+﻿#if WPF
+using Esri.ArcGISRuntime.Mapping.FeatureForms;
+
+namespace Esri.ArcGISRuntime.Toolkit.Primitives
+{
+    /// <summary>
+    /// Picker for the <see cref="DateTimePickerFormInput"/>.
+    /// </summary>
+    [TemplatePart(Name = "DatePicker", Type = typeof(DatePicker))]
+    public class DateTimePickerFormInputView : Control
+    {
+        private DatePicker? _datePicker;
+        private TimePicker? _timePicker;
+
+        /// <summary>
+        /// Initializes an instance of the <see cref="DateTimePickerFormInputView"/> class.
+        /// </summary>
+        public DateTimePickerFormInputView()
+        {
+            DefaultStyleKey = typeof(DateTimePickerFormInputView);
+        }
+
+        /// <inheritdoc />
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _datePicker = GetTemplateChild("DatePicker") as DatePicker;
+            _timePicker = GetTemplateChild("TimePicker") as TimePicker;
+            if (_datePicker != null)
+            {
+                _datePicker.SelectedDateChanged += DatePicker_SelectedDateChanged;
+            }
+            if (_timePicker != null)
+            {
+                _timePicker.TimeChanged += TimePicker_TimeChanged;
+            }
+            ConfigurePickers();
+        }
+
+        private void TimePicker_TimeChanged(object? sender, EventArgs e) => UpdateValue();
+
+        private void DatePicker_SelectedDateChanged(object? sender, SelectionChangedEventArgs e) => UpdateValue();
+
+        private void UpdateValue()
+        { 
+            if (Element?.Input is DateTimePickerFormInput input && _datePicker != null)
+            {
+                var date = _datePicker.SelectedDate;
+                if(date.HasValue && input.IncludeTime && _timePicker != null && _timePicker.Time.HasValue)
+                {
+                    date = date.Value.Subtract(date.Value.TimeOfDay).Add(_timePicker.Time.Value);
+                }
+                if (!object.Equals(Element?.Value, date))
+                    Element?.UpdateValue(date);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the FieldFormElement.
+        /// </summary>
+        public FieldFormElement? Element
+        {
+            get { return (FieldFormElement)GetValue(ElementProperty); }
+            set { SetValue(ElementProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="Element"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ElementProperty =
+            DependencyProperty.Register(nameof(Element), typeof(FieldFormElement), typeof(DateTimePickerFormInputView), new PropertyMetadata(null, (s, e) => ((DateTimePickerFormInputView)s).OnElementPropertyChanged(e.OldValue as FieldFormElement, e.NewValue as FieldFormElement)));
+
+        private void OnElementPropertyChanged(FieldFormElement? oldValue, FieldFormElement? newValue)
+        {
+            if (newValue is not null)
+            {
+                ((System.ComponentModel.INotifyPropertyChanged)newValue).PropertyChanged += Element_PropertyChanged;
+            }
+        }
+
+        private void Element_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(FieldFormElement.Value))
+            {
+                ConfigurePickers();
+            }
+        }
+
+        private bool _rentrancyFlag;
+
+        private void ConfigurePickers()
+        {
+            if (_rentrancyFlag) return;
+            _rentrancyFlag = true;
+            DateTime? selectedDate = (DateTime?)Element?.Value;
+            if (Element?.Input is DateTimePickerFormInput input)
+            {
+                if (_datePicker is not null)
+                {
+                    _datePicker.SelectedDate = selectedDate;
+                    _datePicker.DisplayDateStart = input.Min.HasValue ? input.Min.Value.Date : null;
+                    _datePicker.DisplayDateEnd = input.Max.HasValue ? input.Max.Value.Date : null;
+                }
+                if (_timePicker != null)
+                {
+                    _timePicker.Visibility = input.IncludeTime ? Visibility.Visible : Visibility.Collapsed;
+                    _timePicker.Time = selectedDate.HasValue ? selectedDate.Value.TimeOfDay : null;
+                }
+            }
+            _rentrancyFlag = false;
+        }
+    }
+}
+#endif
---
+++ src/Toolkit/Toolkit/UI/Controls/FeatureForm/TimePicker.cs
@@ -0,0 +1,100 @@
+﻿#if WPF
+using Esri.ArcGISRuntime.Mapping.FeatureForms;
+using System.Globalization;
+using System.Windows.Input;
+
+namespace Esri.ArcGISRuntime.Toolkit.Primitives
+{
+    /// <summary>
+    /// TimePicker used by <see cref="DateTimePickerFormInput"/>.
+    /// </summary>
+    public class TimePicker : Control
+    {
+        private TextBox? _timeText;
+
+        /// <summary>
+        /// Initializes an instance of the <see cref="TimePicker"/> class.
+        /// </summary>
+        public TimePicker()
+        {
+            DefaultStyleKey = typeof(TimePicker);
+        }
+
+        /// <inheritdoc />
+        public override void OnApplyTemplate()
+        {
+            _timeText = GetTemplateChild("TimeText") as TextBox;
+            if(_timeText != null)
+            {
+                _timeText.LostFocus += TimeText_LostFocus;
+                _timeText.KeyDown += TimeText_KeyDown;
+            }
+            UpdateText();
+            base.OnApplyTemplate();
+        }
+
+        private void TimeText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                TryParseText();
+            }
+        }
+
+        private void TimeText_LostFocus(object sender, RoutedEventArgs e) => TryParseText();
+
+        private void TryParseText()
+        {
+            if (_timeText is null)
+                return;
+            if(DateTime.TryParse(_timeText.Text, null, DateTimeStyles.NoCurrentDateDefault, out DateTime result))
+            {
+                Time = result.TimeOfDay;
+            }
+            else
+            {
+                UpdateText(); //Reset text
+            }
+        }
+
+        private void UpdateText()
+        {
+            if (_timeText != null)
+            {
+                if (Time.HasValue)
+                {
+                    _timeText.Text = new DateTime(1, 1, 1).Add(Time.Value).ToString("T");
+                }
+                else
+                    _timeText.Text = null;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the displayed time.
+        /// </summary>
+        public TimeSpan? Time
+        {
+            get { return (TimeSpan?)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+
+        /// <summary>
+        /// Identifies the <see cref="Time"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register(nameof(Time), typeof(TimeSpan?), typeof(TimePicker), new PropertyMetadata(null,((s,e) => ((TimePicker)s).OnTimePropertyChanged())));
+
+        private void OnTimePropertyChanged()
+        {
+            UpdateText();
+            TimeChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Raised when the <see cref="Time"/> property changes
+        /// </summary>
+        public event EventHandler? TimeChanged;
+    }
+}
+#endif
