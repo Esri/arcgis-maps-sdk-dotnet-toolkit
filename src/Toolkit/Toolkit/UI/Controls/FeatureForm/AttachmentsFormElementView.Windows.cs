89b0f4d535ec5bf6a5bef87ff472f844cf129420
dotMorten <no@spam.thx>
2024-06-25 10:30:29 -0700

    Added Add Attachment button

--- src/Toolkit/Toolkit/UI/Controls/FeatureForm/AttachmentsFormElementView.Windows.cs
+++ src/Toolkit/Toolkit/UI/Controls/FeatureForm/AttachmentsFormElementView.Windows.cs
@@ -2,13 +2,18 @@
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Mapping.FeatureForms;
 using Esri.ArcGISRuntime.Toolkit.Internal;
+using Esri.ArcGISRuntime.Toolkit.UI.Controls;
+using Microsoft.Win32;
 using System.ComponentModel;
+using System.IO;
+using System.Windows.Controls.Primitives;
 
 namespace Esri.ArcGISRuntime.Toolkit.Primitives
 {
-    // [TemplatePart(Name ="Selector", Type = typeof(System.Windows.Controls.Primitives.Selector))]
+    [TemplatePart(Name = "AddAttachmentButton", Type = typeof(ButtonBase))]
     public partial class AttachmentsFormElementView : Control
     {
+        private ButtonBase? _addAttachmentButton;
         /// <inheritdoc />
 #if WINDOWS_XAML
         protected override void OnApplyTemplate()
@@ -17,6 +22,30 @@ namespace Esri.ArcGISRuntime.Toolkit.Primitives
 #endif
         {
             base.OnApplyTemplate();
+            if (_addAttachmentButton is not null)
+            {
+                _addAttachmentButton.Click -= AddAttachmentButton_Click;
+            }
+            _addAttachmentButton = GetTemplateChild("AddAttachmentButton") as ButtonBase;
+            if(_addAttachmentButton is not null)
+            {
+                _addAttachmentButton.Click += AddAttachmentButton_Click;
+            }
+        }
+
+        private void AddAttachmentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Element is null) return;
+            // TODO: Allow overriding what happens here
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var fileInfo = new FileInfo(openFileDialog.FileName);
+                if (fileInfo.Exists)
+                {
+                    Element.AddAttachment(fileInfo.Name, MimeTypeMap.GetMimeType(fileInfo.Extension), File.ReadAllBytes(fileInfo.FullName));
+                }
+            }
         }
     }
 }
