c6f02157ad19a3c11570066153936cd1a4521ce1
dotMorten <mn@iter.dk>
2025-03-25 13:12:21 -0700

    Adds AoT support to BookmarksView and BasemapGallery

---
+++ src/Toolkit/Toolkit.WinUI/UI/Controls/BasemapGallery/BasemapGallery.Theme.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Esri.ArcGISRuntime.Toolkit;
+
+internal sealed partial class BasemapGalleryResources : ResourceDictionary
+{
+    public BasemapGalleryResources()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Toolkit/Toolkit.WinUI/UI/Controls/BookmarksView/BookmarksView.Theme.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace Esri.ArcGISRuntime.Toolkit;
+
+internal sealed partial class BookmarksViewResources : ResourceDictionary
+{
+    public BookmarksViewResources()
+    {
+        InitializeComponent();
+    }
+}
