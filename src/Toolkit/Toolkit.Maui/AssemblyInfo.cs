9b5bc4928ac5dbf277cc80293f0065d660004651
Morten Nielsen <mort5161@esri.com>
2025-04-08 15:34:15 -0700

    Update min versions

--- src/Toolkit/Toolkit.Maui/AssemblyInfo.cs
+++ src/Toolkit/Toolkit.Maui/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 ﻿#if !__IOS__ && !WINDOWS && !__ANDROID__ && !__MACCATALYST__
 // Make sure net6.0 non-os target clearly states which platforms are supported by the toolkit
 [assembly: System.Runtime.Versioning.SupportedOSPlatform("windows10.0.19041")]
-[assembly: System.Runtime.Versioning.SupportedOSPlatform("android26.0")]
-[assembly: System.Runtime.Versioning.SupportedOSPlatform("ios14.0")]
-[assembly: System.Runtime.Versioning.SupportedOSPlatform("maccatalyst14.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("android28.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("ios17.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatform("maccatalyst17.0")]
 #endif
 
 [assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://schemas.esri.com/arcgis/runtime/2013", "esri")]
