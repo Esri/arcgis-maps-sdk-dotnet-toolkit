70f74fe8be31a337aaa9e2965c0e4ec899efb570
Prathamesh Narkhede <pnarkhede@esri.com>
2025-07-16 18:46:24 -0700

    Updating DocFX version and .Net versions

--- docs/BuildDoc.cmd
+++ docs/BuildDoc.cmd
@@ -1,18 +1,18 @@
 @ECHO OFF
 
-SET DocFXVersion=2.59.4
+SET DocFXVersion=2.78.3
 SET DocFxFolder=%~dp0..\.tools\docfx
 
 REM Download DocFx
 
 IF NOT EXIST "%DocFxFolder%\v%DocFXVersion%\docfx.exe" (
    MKDIR "%DocFXFolder%\v%DocFXVersion%"
-   powershell -ExecutionPolicy ByPass -command "Invoke-WebRequest -Uri "https://github.com/dotnet/docfx/releases/download/v%DocFXVersion%/docfx.zip" -OutFile '%DocFxFolder%\docfx_v%DocFXVersion%.zip'"
+   powershell -ExecutionPolicy ByPass -command "Invoke-WebRequest -Uri "https://github.com/dotnet/docfx/releases/download/v%DocFXVersion%/docfx-win-x64-v%DocFXVersion%.zip" -OutFile '%DocFxFolder%\docfx_v%DocFXVersion%.zip'"
    powershell -ExecutionPolicy ByPass -command "Expand-Archive -LiteralPath '%DocFxFolder%\docfx_v%DocFXVersion%.zip' -DestinationPath '%DocFxFolder%\v%DocFXVersion%'"
    DEL "%DocFxFolder%\docfx_v%DocFXVersion%.zip" /Q
 )
 REM Build the output site (HTML) from the generated metadata and input files (uses configuration in docfx.json in this folder)
-%DocFxFolder%\v%DocFXVersion%\docfx.exe %~dp0\docfx.json --force
+%DocFxFolder%\v%DocFXVersion%\docfx.exe %~dp0\docfx.json
 ECHO Fixing API Reference Links
 powershell -ExecutionPolicy ByPass -command "%~dp0FixApiRefLinks" -Path %~dp0..\Output\docs_site\api\
 start http://localhost:8080
