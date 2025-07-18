1c94b6353a35e9582dc5136f06254c7721934b30
Matvei Stefarov <mstefarov@esri.com>
2025-07-18 13:57:22 -0700

    Fix header spacing near "navigate back" button

--- src/Toolkit/Toolkit/UI/Controls/PopupViewer/NavigationSubView.cs
+++ src/Toolkit/Toolkit/UI/Controls/PopupViewer/NavigationSubView.cs
@@ -61,9 +61,6 @@ namespace Esri.ArcGISRuntime.Toolkit.Primitives
 #else
                 back.Visibility = _navigationStack.Count > 0 ? Visibility.Visible : Visibility.Collapsed;
 #endif
-#if WPF
-                back.Margin = _navigationStack.Count > 1 ? new Thickness() : new Thickness(0, 0, 10, 0);
-#endif
             }
             if (GetTemplateChild("NavigateUp") is FrameworkElement up)
             {
