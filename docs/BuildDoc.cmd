c387b3a935e8f42ea2dc1b4dea940d9b88885d9e
Prathamesh Narkhede <pnarkhede@esri.com>
2025-07-18 17:49:01 -0700

    Restoring foldername Removing creating folders by docfx versions

--- docs/BuildDoc.cmd
+++ docs/BuildDoc.cmd
@@ -5,18 +5,18 @@ SET DocFxFolder=%~dp0..\.tools\docfx
 
 REM Download DocFx
 
-IF NOT EXIST "%DocFxFolder%\v%DocFXVersion%\docfx.exe" (
-   MKDIR "%DocFXFolder%\v%DocFXVersion%"
-   powershell -ExecutionPolicy ByPass -command "Invoke-WebRequest -Uri "https://github.com/dotnet/docfx/releases/download/v%DocFXVersion%/docfx-win-x64-v%DocFXVersion%.zip" -OutFile '%DocFxFolder%\docfx_v%DocFXVersion%.zip'"
-   powershell -ExecutionPolicy ByPass -command "Expand-Archive -LiteralPath '%DocFxFolder%\docfx_v%DocFXVersion%.zip' -DestinationPath '%DocFxFolder%\v%DocFXVersion%'"
-   DEL "%DocFxFolder%\docfx_v%DocFXVersion%.zip" /Q
+IF NOT EXIST "%DocFxFolder%\docfx.exe" (
+   MKDIR "%DocFXFolder%"
+   powershell -ExecutionPolicy ByPass -command "Invoke-WebRequest -Uri "https://github.com/dotnet/docfx/releases/download/v%DocFXVersion%/docfx-win-x64-v%DocFXVersion%.zip" -OutFile '%DocFxFolder%\docfx.zip'"
+   powershell -ExecutionPolicy ByPass -command "Expand-Archive -LiteralPath '%DocFxFolder%\docfx.zip' -DestinationPath '%DocFxFolder%'"
+   DEL "%DocFxFolder%\docfx.zip" /Q
 )
 REM Build the output site (HTML) from the generated metadata and input files (uses configuration in docfx.json in this folder)
-%DocFxFolder%\v%DocFXVersion%\docfx.exe %~dp0\docfx.json
+%DocFxFolder%\docfx.exe %~dp0\docfx.json
 ECHO Fixing API Reference Links
 powershell -ExecutionPolicy ByPass -command "%~dp0FixApiRefLinks" -Path %~dp0..\Output\docs_site\api\
 start http://localhost:8080
-%DocFxFolder%\v%DocFXVersion%\docfx.exe serve %~dp0..\Output\docs_site\
+%DocFxFolder%\docfx.exe serve %~dp0..\Output\docs_site\
 
 REM Publishing doc:
 REM cd %~dp0..\Output\docs_site
