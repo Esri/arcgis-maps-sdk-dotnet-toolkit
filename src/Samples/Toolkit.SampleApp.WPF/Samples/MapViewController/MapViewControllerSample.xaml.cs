2b9dbdb1470c23d3f5d5c4c74fac9c9d3a6e2e75
Prathamesh Narkhede <pnarkhede@esri.com>
2025-07-25 20:15:33 -0700

    Added MapViewController sample

---
+++ src/Samples/Toolkit.SampleApp.WPF/Samples/MapViewController/MapViewControllerSample.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.UI;
+using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Esri.ArcGISRuntime.Toolkit.Samples.MapViewController
+{
+    [SampleInfo(ApiKeyRequired = true)]
+    public partial class MapViewControllerSample : UserControl
+    {
+        public MapViewControllerSample()
+        {
+            InitializeComponent();
+            CreateInitialGraphics();
+        }
+
+        private void CreateInitialGraphics()
+        {
+            var polygonPoints = new List<MapPoint>
+            {
+                new(-117.1956, 34.0565),
+                new(-117.1910, 34.0565),
+                new(-117.1910, 34.0600),
+                new(-117.1933, 34.0610),
+                new(-117.1956, 34.0600),
+                new(-117.1956, 34.0565)
+            };
+            var demoPolygon = new Polygon(polygonPoints, SpatialReferences.Wgs84);
+            var polygonGraphic = new Graphic(demoPolygon, new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, System.Drawing.Color.Red, 2));
+
+            // Add a new vertex to the polyline
+            var polylinePoints = new List<MapPoint>
+            {
+                new(-117.1956, 34.0565),
+                new(-117.1934, 34.0575),
+                new(-117.1910, 34.0565),
+                new(-117.1920, 34.0580)
+            };
+            var demoPolyline = new Polyline(polylinePoints, SpatialReferences.Wgs84);
+            var polylineGraphic = new Graphic(demoPolyline, new SimpleLineSymbol(SimpleLineSymbolStyle.Dash, System.Drawing.Color.Blue, 2));
+
+            var graphicsOverlay = new GraphicsOverlay();
+            graphicsOverlay.Graphics.Add(polygonGraphic);
+            graphicsOverlay.Graphics.Add(polylineGraphic);
+
+            var pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, System.Drawing.Color.Green, 10);
+            foreach (var pt in polygonPoints)
+            {
+                var pointGraphic = new Graphic(pt, pointSymbol);
+                graphicsOverlay.Graphics.Add(pointGraphic);
+            }
+
+            MyMapView.GraphicsOverlays.Add(graphicsOverlay);
+
+
+            MyMapView.SetViewpoint(new Mapping.Viewpoint(demoPolygon.Extent));
+        }
+    }
+}
---
+++ src/Samples/Toolkit.SampleApp.WPF/Samples/MapViewController/MapViewControllerSampleVM.cs
@@ -0,0 +1,84 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Location;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI.Controls;
+using Esri.ArcGISRuntime.UI.Editing;
+
+namespace Esri.ArcGISRuntime.Toolkit.Samples.MapViewController;
+
+public partial class MapViewControllerSampleVM
+{
+    public Map Map { get; } = new Map(BasemapStyle.OpenStreets);
+
+    public MyMapViewController Controller { get; } = new MyMapViewController();
+
+    [RelayCommand]
+    public async Task OnGeoViewTapped(GeoViewInputEventArgs eventArgs) => await Identify(eventArgs.Position, eventArgs.Location);
+
+    [RelayCommand]
+    public void OnToggleGeometryEditor()
+    {
+        if (Controller.GeometryEditor?.Stop() is null)
+        {
+            Controller.GeometryEditor?.Start(GeometryType.Point);
+            if (Controller.GeometryEditor?.SnapSettings is SnapSettings snapSettings)
+            {
+                snapSettings.IsEnabled = true;
+            }
+        }
+    }
+
+    public async Task Identify(Point location, MapPoint? mapLocation)
+    {
+        try
+        {
+            var result = await Controller.IdentifyGeometryEditorAsync(location, 10);
+
+            if (result is IdentifyGeometryEditorResult editorResult)
+            {
+                var summary = $"Selected {result.Elements.Count} element(s):\n" +
+                              string.Join("\n", result.Elements.Select(e => e.GetType().Name));
+
+                MapPoint? calloutLocation = null;
+                if (result.Elements.FirstOrDefault() is GeometryEditorElement firstElem)
+                    calloutLocation = firstElem switch
+                    {
+                        GeometryEditorVertex pt => pt.Point,
+                        GeometryEditorPart part => part.Part.StartPoint,
+                        GeometryEditorMidVertex midVertex => midVertex.Point,
+                        GeometryEditorGeometry geometry => geometry.Geometry?.Extent?.GetCenter(),
+                        _ => null
+                    };
+
+                calloutLocation ??= mapLocation;
+
+                if (calloutLocation != null)
+                    Controller.ShowCalloutAt(calloutLocation, new Esri.ArcGISRuntime.UI.CalloutDefinition(summary));
+            }
+            else if (mapLocation is not null)
+            {
+                Controller.ShowCalloutAt(mapLocation, new Esri.ArcGISRuntime.UI.CalloutDefinition("No Geometry found"));
+            }
+        }
+        catch (ArcGISException ex)
+        {
+            Controller.ShowCalloutAt(mapLocation, new Esri.ArcGISRuntime.UI.CalloutDefinition("Failed to identfy Geometry", ex.Message));
+        }
+    }
+}
+
+public class MyMapViewController : UI.MapViewController
+{
+    public GeometryEditor? GeometryEditor => ConnectedView?.GeometryEditor;
+}
