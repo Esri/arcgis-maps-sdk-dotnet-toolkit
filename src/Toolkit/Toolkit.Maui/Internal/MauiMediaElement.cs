db35ba85a038be7f4d1877bf07f4a3a9e97e93ba
Prathamesh Narkhede <pnarkhede@esri.com>
2025-09-15 20:20:21 -0700

    Enhance MauiMediaElement with documentation and fixes Changed MauiMediaElement declaration to a partial class Enhanced the Dispose method to properly remove media items

--- src/Toolkit/Toolkit.Maui/Internal/MauiMediaElement.cs
+++ src/Toolkit/Toolkit.Maui/Internal/MauiMediaElement.cs
@@ -1,6 +1,9 @@
 ﻿namespace Esri.ArcGISRuntime.Toolkit.Maui.Internal;
 
-internal class MauiMediaElement : View
+/// <summary>
+/// A view that displays audio or video media from a specified source.
+/// </summary>
+internal partial class MauiMediaElement : View
 {
     public static readonly BindableProperty SourceProperty =
         BindableProperty.Create(nameof(Source), typeof(Uri), typeof(MauiMediaElement), null);
--- src/Toolkit/Toolkit.Maui/Internal/MauiMediaElementHandler.iOS.cs
+++ src/Toolkit/Toolkit.Maui/Internal/MauiMediaElementHandler.iOS.cs
@@ -19,22 +19,20 @@ internal partial class MauiMediaElementHandler : ViewHandler<MauiMediaElement, U
         _player = new AVPlayer();
         _playerController = new AVPlayerViewController
         {
-            Player = _player
+            Player = _player,
         };
-
         _playerView = _playerController.View;
-        _playerView.Frame = new CGRect(0, 0, 320, 180); // Set initial frame
+        _playerView.Frame = new CGRect(0, 0, 320, 180); // Default size, The aspect ratio is still maintained if video has different dimensions
 
         return _playerView;
     }
 
     public void UpdateSource(Uri source)
     {
-        if (source != null)
+        if (source is not null)
         {
             var url = NSUrl.FromString(source.ToString());
             _player.ReplaceCurrentItemWithPlayerItem(new AVPlayerItem(url));
-            _player.Play();
         }
     }
 
@@ -53,10 +51,12 @@ internal partial class MauiMediaElementHandler : ViewHandler<MauiMediaElement, U
     {
         // Cleanup resources
         _player?.Pause();
+        _player.ReplaceCurrentItemWithPlayerItem(null);
         _player?.Dispose();
         _player = null;
         _playerController?.Dispose();
         _playerController = null;
+        _playerView?.RemoveFromSuperview();
         _playerView?.Dispose();
         _playerView = null;
     }
