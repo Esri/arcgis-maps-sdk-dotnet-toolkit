14bf095ac288524eb64d3f48d266ba2eb23fb44f
Prathamesh Narkhede <55591622+prathameshnarkhede@users.noreply.github.com>
2025-10-31 10:22:59 -0700

    Update obsolete credential creation method in samples (#703)
    * Update obsolete credential creation method in samples
    
    * Restoring name

--- src/Samples/Toolkit.SampleApp.WPF/Samples/UtilityNetworkTraceTool/UNTraceSimple.xaml.cs
+++ src/Samples/Toolkit.SampleApp.WPF/Samples/UtilityNetworkTraceTool/UNTraceSimple.xaml.cs
@@ -22,7 +22,7 @@ namespace Esri.ArcGISRuntime.Toolkit.Samples.UtilityNetworkTraceTool
             try
             {
                 // Using public credentials from https://developers.arcgis.com/javascript/latest/sample-code/widgets-untrace/
-                var portal1Credential = await AuthenticationManager.Current.GenerateCredentialAsync(new Uri("https://sampleserver7.arcgisonline.com/portal/sharing/rest"), "viewer01", "I68VGU^nMurF");
+                var portal1Credential = await AccessTokenCredential.CreateAsync(new Uri("https://sampleserver7.arcgisonline.com/portal/sharing/rest"), "viewer01", "I68VGU^nMurF");
                 AuthenticationManager.Current.AddCredential(portal1Credential);
 
                 MyMapView.Map = new Map(new Uri(WebmapURL));
