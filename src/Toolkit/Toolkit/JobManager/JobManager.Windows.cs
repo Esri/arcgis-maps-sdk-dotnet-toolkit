15f51e89a36156f122a3328352b415ee8a7f80d4
Morten Nielsen <mort5161@esri.com>
2025-12-10 17:30:00 -0800

    Adds a job manager for managing and resuming jobs

---
+++ src/Toolkit/Toolkit/JobManager/JobManager.Windows.cs
@@ -0,0 +1,186 @@
+﻿#if WINDOWS
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Esri.ArcGISRuntime.Tasks;
+#if WINUI || MAUI
+using Microsoft.Windows.AppNotifications;
+using Microsoft.Windows.AppNotifications.Builder;
+#endif
+using Windows.ApplicationModel.Background;
+using Windows.Storage;
+using Windows.System.Threading;
+
+namespace Esri.ArcGISRuntime.Toolkit
+{
+    public sealed partial class JobManager
+    {
+        const string classGuid = "D998B238-C096-4181-B971-9CD1EB760547";
+
+        private JobManager(string? id)
+        {
+            _id = id;
+            Init();
+#if WINUI || MAUI // Windows App SDK is required for background tasks
+            //TODO RegisterBackgroundTask();
+#endif
+        }
+
+#if WINUI || MAUI
+		private void RegisterBackgroundTask()
+        {
+            
+            var builder = new BackgroundTaskBuilder();
+            //var trigger = new SystemTrigger(SystemTriggerType.TimeZoneChange, false);
+            //var backgroundTrigger = trigger as IBackgroundTrigger;
+            //builder.SetTrigger(backgroundTrigger);
+            var trigger = new TimeTrigger(15, false);
+            builder.SetTrigger(trigger);
+            builder.AddCondition(new SystemCondition(SystemConditionType.InternetAvailable));
+            builder.SetTaskEntryPointClsid(new Guid(classGuid));
+            builder.Register();
+
+            var notification = new AppNotificationBuilder()
+.AddText("Background registered")
+.BuildNotification();
+
+            AppNotificationManager.Default.Show(notification);
+        }
+#endif
+        private const int MaxLength = 4090;
+
+        private void SaveState(string? json)
+        {
+#if WPF
+            throw new NotImplementedException("Job persistence is not implemented for WPF applications."); // TODO
+#endif
+            var localSettings = ApplicationData.Current.LocalSettings;
+            if (localSettings.Containers.ContainsKey(DefaultsKey))
+                localSettings.DeleteContainer(DefaultsKey);
+            var entries = json?.Split('\n',StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            if(entries is null || entries.Length == 0)
+            {
+                return;
+            }
+            var container = localSettings.CreateContainer(DefaultsKey, ApplicationDataCreateDisposition.Always);
+            int i = 0;
+            foreach (var entry in entries)
+            {
+                var jsonEntry = entry;
+                if(jsonEntry.Length > MaxLength)
+                {
+                    while(jsonEntry.Length > MaxLength)
+                    {   
+                        var chunk = jsonEntry.Substring(0, MaxLength - 1);
+                        container.Values[(i++).ToString()] = chunk + '\n';
+                        jsonEntry = jsonEntry.Substring(MaxLength - 1);
+                    }
+                }
+                container.Values[(i++).ToString()] = jsonEntry;
+            }
+        }
+
+        partial void OnJobCollectionChanged()
+        {
+            SaveState();
+        }
+
+        private string? LoadStateInternal()
+        {
+            var localSettings = ApplicationData.Current.LocalSettings;
+            if (localSettings.Containers.TryGetValue(DefaultsKey, out var container))
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in container.Values.Keys.OrderBy(k=>int.Parse(k)))
+                {
+                    var value = container.Values[item];
+                    if (value is string json)
+                    {
+                        var lastchar = json[json.Length - 1];
+                        sb.Append(json.TrimEnd());
+                        if (lastchar != '\n')
+                            sb.Append('\n');
+                    }
+                }
+                return sb.ToString();
+            }
+            return null;
+        }
+
+#if WINUI || MAUI
+        [ComVisible(true)]
+        [ClassInterface(ClassInterfaceType.None)]
+        [Guid(classGuid)]
+        [ComSourceInterfaces(typeof(IBackgroundTask))]
+        public sealed class BackgroundTask : IBackgroundTask
+        {
+            private BackgroundTaskDeferral? _deferral = null;
+            private volatile bool _cancelRequested = false;
+            private ThreadPoolTimer? _periodicTimer = null;
+            private int _progress = 0;
+            private JobManager? manager;
+            private IBackgroundTaskInstance? _taskInstance;
+            [MTAThread]
+            public void Run(IBackgroundTaskInstance taskInstance)
+            {
+                
+                var taskId = taskInstance.Task.Name;
+                manager = JobManager.Create(taskId);
+                if(manager.HasRunningJobs == false)
+                {
+                    return;
+                }
+                // Get deferral to indicate not to kill the background task process as soon as the Run method returns
+                _deferral = taskInstance.GetDeferral();
+                // Wire the cancellation handler.
+                taskInstance.Canceled += this.OnCanceled;
+
+                // Set the progress to indicate this task has started
+                taskInstance.Progress = 0;
+                var notification = new AppNotificationBuilder()
+                    .AddText("Background task launched!")
+                    .BuildNotification();
+
+                AppNotificationManager.Default.Show(notification);
+
+                _periodicTimer = ThreadPoolTimer.CreatePeriodicTimer(new TimerElapsedHandler(PeriodicTimerCallback), TimeSpan.FromSeconds(30));
+            }
+
+            private async void PeriodicTimerCallback(ThreadPoolTimer timer)
+            {
+                if (manager is not null && manager.Jobs.Count > 0)
+                {
+                    if (!_cancelRequested)
+                    {
+                        try
+                        {
+                            await manager.PerformStatusChecks();
+                            manager.SaveState();
+                            var progress = manager.Jobs.Sum(j => j.Progress) / (double)manager.Jobs.Count;
+                            if (_taskInstance is not null)
+                            {
+                                _taskInstance.Progress = (uint)progress;
+                            }
+                            var notification = new AppNotificationBuilder().AddText("Progress " + progress.ToString() + "%").BuildNotification();
+                            AppNotificationManager.Default.Show(notification);
+                        }
+                        catch { }
+                    }
+                }
+                if (manager is null || manager.HasRunningJobs)
+                    _deferral?.Complete();
+
+                //BackgroundTaskBuilder.MainWindow.taskStatus(_progress);
+            }
+
+            private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+            {
+                // Handle cancellation operations and flag the task to end
+                _cancelRequested = true;
+            }
+        }
+#endif
+    }
+}
+#endif
---
+++ src/Toolkit/Toolkit/JobManager/JobManager.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Esri.ArcGISRuntime.Tasks;
+
+namespace Esri.ArcGISRuntime.Toolkit
+{
+    /// <summary>An object that manages saving and loading jobs so that they can continue to run if the
+    /// app is backgrounded or even terminated.</summary>
+    /// <remarks>
+    /// <para>
+    /// The job manager is instantiable, but the ``shared`` instance is suitable for most applications.
+    ///</para><para>
+    /// <b>Background</b><br/>
+    /// Jobs are long running server operations. When a job instance is started on the client,
+    /// it makes a request to a service asking it to start work on the server. At that point, the client
+    /// polls the server intermittently to check the status of the work. Once the work is completed
+    /// the result is downloaded with a background `URLSession`. This allows the download to
+    /// complete out of process, and the download task can relaunch the app upon completion, even
+    /// in the case where the app was terminated.
+    ///</para><para>
+    /// We do not expect users to keep an application in the foreground and wait for a job to complete.
+    /// Once the job is started, if the app is backgrounded, we can use an app refresh
+    /// background task to check the status of the work on the server. If the server work is complete
+    /// we can start downloading the result in the background at that point. If the work on the server
+    /// is not complete, we can reschedule another background app refresh to recheck status.
+    ///</para><para>
+    /// There is some iOS behavior to be aware of as well. In iOS, if an application is backgrounded,
+    /// the operating system can terminate the app at its discretion. This means that jobs need to be
+    /// serialized when an app is backgrounded so that if the app is terminated the jobs can be
+    /// rehydrated upon relaunch of the app.
+    ///</para><para>
+    /// Also, in iOS, if the user of an app removes the app from the multitasking UI (aka force quits it),
+    /// the system interprets this as a strong indication that the app should
+    /// do no more work in the background. The consequences of this are two-fold for jobs.
+    /// One, any background fetch tasks are not given any time until the app is relaunched again.
+    /// And two, any background downloads that are in progress are canceled by the operating system.
+    ///</para><para>
+    /// <b>Features</b><br/>
+    /// The job manager is an `ObservableObject` with a mutable ``jobs`` property. Adding a job to this
+    /// property will allow the job manager to do the work to make sure that we can rehydrate a job
+    /// if an app is terminated.
+    ///</para><para>
+    /// As such, the job manager will:
+    /// <list>
+    ///  <item>Serialize the job to the user defaults when the app is backgrounded.</item>
+    ///  <item>Deserialize the job when an application is relaunched.</item>
+    ///  </list>
+    ///</para><para>
+    /// The job manager will help with the lifetime of jobs in other ways as well.
+    ///</para><para>
+    /// <b>iOS specific behavior:</b>
+    /// The job manager will ask the system for some background processing time when an app
+    /// is backgrounded so that jobs that are not yet started on the server, can have some time to
+    /// allow them to start. This means if you kick off a job and it hasn't actually started on the server
+    /// when the app is backgrounded, the job should have enough time to start on the server which
+    /// will cause it to enter into a polling state. When the job reaches the polling state the
+    /// status of the work on the server can be checked intermittently.
+    ///</para><para>
+    /// To enable polling while an app is backgrounded, the job manager will request from the system a
+    /// background refresh task (if enabled via the <c>JobManager.PreferredBackgroundStatusCheckSchedule</c>
+    /// property). If the system later executes the background refresh task then the
+    /// job manager will check the status of any running jobs. At that point the jobs may start
+    /// downloading their result. Note, this does not work on the simulator, this behavior can only
+    /// be tested on an actual device.
+    ///</para><para>
+    /// Now that the job can check status in the background, it can start downloading in the background.
+    /// By default, jobs will download their results with background URL session. This means that the
+    /// download can execute out of process, even if the app is terminated. If the app is terminated and
+    /// then later relaunched by the system because a background downloaded completed, then you may
+    /// call the <c>JobManager.ResumeAllPausedJobs()</c> method from the application relaunch point,
+    /// which will correlate the jobs to their respective downloads that completed and the jobs will
+    /// then finish. The app relaunch point can happen via the SwiftUI modifier `.backgroundTask(.urlSession(...))`.
+    /// In UIKit it would be the `UIApplicationDelegate` method `func application(UIApplication, handleEventsForBackgroundURLSession: String, completionHandler: () -> Void)`
+    /// </para>
+    /// </remarks>
+    public sealed partial class JobManager
+    {
+        /// <summary>
+        /// Gets the shared job manager.
+        /// </summary>
+        public static JobManager Shared { get; } = new JobManager(null);
+
+        private string? _id;
+
+        // The key for which state will be serialized under the user defaults.
+        private string DefaultsKey =>
+            _id != null
+                ? $"com.esri.ArcGISToolkit.jobManager.{_id}.statusCheck"
+                : "com.esri.ArcGISToolkit.jobManager.statusCheck";
+
+        /// <summary>Creates a job manager with a unique id.</summary>
+        /// <remarks><para>This initializer allows you to create a specific instance of a job manager
+        /// for cases when you don't want to take over the shared job manager instance.
+        ///</para><para>
+        /// The provided ID should be unique to a specific purpose in your application.
+        /// On each successive run of the app, you must re-use the same id when you initialize
+        /// your job manager for it to be able to properly reload its state.
+        ///</para><para>
+        /// If you create multiple instances with the same id the behavior is undefined.
+        ///</para></remarks>
+        /// <param name="id">The unique identifier for this job manager.</param>
+        public static JobManager Create(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                throw new ArgumentException("id cannot be null or whitespace", nameof(id));
+
+            return new JobManager(id);
+        }
+
+        private void Init()
+        {
+            var jobCollection = new System.Collections.ObjectModel.ObservableCollection<IJob>();
+            Jobs = jobCollection;
+            LoadState();
+            foreach (var job in Jobs)
+            {
+                job.StatusChanged += Job_StatusChanged;
+                job.ProgressChanged += Job_ProgressChanged;
+            }
+            jobCollection.CollectionChanged += JobCollection_CollectionChanged;
+        }
+
+        private void Job_ProgressChanged(object? sender, EventArgs e)
+        {
+            OnJobCollectionChanged();
+        }
+
+        private void JobCollection_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            foreach(var job in e.NewItems?.OfType<IJob>() ?? Array.Empty<IJob>())
+            {
+                job.StatusChanged += Job_StatusChanged;
+                job.ProgressChanged += Job_ProgressChanged;
+            }
+            foreach (var job in e.OldItems?.OfType<IJob>() ?? Array.Empty<IJob>())
+            {
+                job.StatusChanged -= Job_StatusChanged;
+                job.ProgressChanged -= Job_ProgressChanged;
+            }
+            OnJobCollectionChanged();
+        }
+
+        private void Job_StatusChanged(object? sender, JobStatus e)
+        {
+#if WINODWS
+            SaveState();
+#endif
+        }
+
+        partial void OnJobCollectionChanged();
+
+        /// <summary>
+        /// The jobs being managed by the job manager.
+        /// </summary>
+        public IList<IJob> Jobs { get; private set; }
+
+
+        // A Boolean value indicating if there are jobs running.
+        private bool HasRunningJobs => Jobs.Any(j => j.Status == JobStatus.Started);
+
+        /// <summary>
+        /// Check the status of all managed jobs.
+        /// </summary>
+        /// <returns></returns>
+        public async Task PerformStatusChecks()
+        {
+            var tasks = Jobs.Select(async job =>
+            {
+                try { await job.CheckStatusAsync(); } catch { }
+            });
+            await Task.WhenAll(tasks);
+        }
+
+        /// <summary>
+        /// Resumes all paused jobs.
+        /// </summary>
+        public void ResumeAllPausedJobs()
+        {
+            // Make sure the default background URLSession is re-created here
+            // in case this method is called from an app relaunch due to background downloads
+            // completed for a terminated app. We need the session to be re-created in that case.
+            // TODO ?? _ = ArcGISEnvironment.backgroundURLSession;
+
+            foreach (var job in Jobs.Where(j => j.Status == JobStatus.Paused))
+            {
+                job.Start();
+            }
+        }
+
+        private object StateLock = new object();
+
+        /// <summary>
+        /// Saves the current job state
+        /// </summary>
+        public void SaveState()
+        {
+            var array = Jobs.Select(j => j.ToJson()).ToArray();
+            lock (StateLock)
+            {
+                SaveState(string.Join("\n", array));
+            }
+        }
+
+        private void LoadState()
+        {
+
+            lock (StateLock)
+            {
+                string? data = LoadStateInternal();
+                if (!string.IsNullOrEmpty(data))
+                {
+                    Jobs.Clear();
+                    foreach (var job in data.Split('\n')
+                        .Select(json =>
+                        {
+                            try { return IJob.FromJson(json)!; }
+                            catch { return null; }
+                        })
+                        .Where(j => j != null))
+                        Jobs.Add(job);
+                }
+            }
+        }
+    }
+}
