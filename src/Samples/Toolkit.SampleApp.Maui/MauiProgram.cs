40b18437f66266b48a3816f45c58ab7f0e79ee55
imalcolm <imalcolm@esri.com>
2026-01-02 12:44:24 -0800

    Added fix for Maui WinUI automation properties not showing on some elements

--- src/Samples/Toolkit.SampleApp.Maui/MauiProgram.cs
+++ src/Samples/Toolkit.SampleApp.Maui/MauiProgram.cs
@@ -15,7 +15,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			}).UseArcGISRuntime().UseArcGISToolkit().UseMauiCommunityToolkit();
+			}).UseWindowsAutomationTreeFix().UseArcGISRuntime().UseArcGISToolkit().UseMauiCommunityToolkit();
 
 		return builder.Build();
 	}
