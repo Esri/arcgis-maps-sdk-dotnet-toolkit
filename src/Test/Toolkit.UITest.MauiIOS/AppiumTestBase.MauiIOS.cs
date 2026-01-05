93eea8f971cfe6b2bfc0c7e989b84fe4eda97df8
ian13774 <ian13774@esri.com>
2026-01-05 10:24:33 -0800

    iOS ScaleLine test now runs

--- src/Test/Toolkit.UITest.MauiIOS/AppiumTestBase.MauiIOS.cs
+++ src/Test/Toolkit.UITest.MauiIOS/AppiumTestBase.MauiIOS.cs
@@ -5,6 +5,16 @@ internal abstract partial class AppiumTestBase
     protected void OpenSample(string sampleName)
     {
         // TODO: Modify the Sample app to allow a more generic means to navigate to particular samples
-        throw new NotImplementedException("OpenSample is not yet implemented for Maui iOS tests.");
+        switch (sampleName)
+        {
+            case "ScaleLine":
+                FindElementByName("Scale Line ").Click();
+                break;
+            case null:
+            case "":
+                throw new ArgumentException("Sample name cannot be null or empty.", nameof(sampleName));
+            default:
+                throw new ArgumentException("Navigation for sample is not implemented. Consider modifying the Sample app to allow a more generic means to navigate to particular samples, then rewrite this method.", nameof(sampleName));
+        }
     }
 }
