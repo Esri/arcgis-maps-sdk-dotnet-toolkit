bf850820f67d62e80b9df4e6386a1dd56f2fabcc
imalcolm <imalcolm@esri.com>
2026-01-25 17:47:39 -0800

    Transfered WPF ScaleLine test to MSTest

---
+++ src/Tests/UITests/Toolkit.UITests.Shared/Appium/AppiumTestBase.Utils.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium.Appium;
+using System.Collections.ObjectModel;
+
+namespace Toolkit.UITest.Shared;
+
+public abstract partial class AppiumTestBase
+{
+    protected ReadOnlyCollection<AppiumElement> FindElements(string name)
+    {
+#if WINDOWS_TEST
+        return Driver.FindElements(MobileBy.AccessibilityId(name));
+#else
+        return Driver.FindElements(MobileBy.Id(name));
+#endif
+    }
+
+    protected AppiumElement FindElement(string id)
+    {
+#if WINDOWS_TEST
+        return Driver.FindElement(MobileBy.AccessibilityId(id));
+#else
+        return Driver.FindElement(MobileBy.Id(id));
+#endif
+    }
+
+    protected AppiumElement FindElement(AppiumElement parent, string id)
+    {
+#if WINDOWS_TEST
+        return parent.FindElement(MobileBy.AccessibilityId(id));
+#else
+        return parent.FindElement(MobileBy.Id(id));
+#endif
+    }
+
+    protected AppiumElement FindElementByName(string name)
+    {
+#if ANDROID_TEST
+        return Driver.FindElement(MobileBy.AndroidUIAutomator($"new UiSelector().text(\"{name}\")"));
+#else
+        return Driver.FindElement(MobileBy.Name(name));
+#endif
+    }
+
+    protected string GetElementText(AppiumElement element)
+    {
+#if WINDOWS_TEST
+        return element.GetAttribute("Name");
+#elif ANDROID_TEST
+        return element.GetAttribute("text");
+#elif MAC_TEST || IOS_TEST
+        return element.GetAttribute("label");
+#else
+        throw new NotImplementedException("FindElement(AppiumElement,string) is not implemented for this platform.");
+#endif
+    }
+}
---
+++ src/Tests/UITests/Toolkit.UITests.Wpf/AppiumTestBase.Wpf.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace Toolkit.UITest.Shared;
+
+public abstract partial class AppiumTestBase
+{
+    protected void OpenSample(string sampleName)
+    {
+        // TODO: Modify the Sample app to allow a more generic means to navigate to particular samples
+        switch (sampleName)
+        {
+            case "ScaleLine":
+                FindElementByName("ScaleLine").Click();
+                FindElementByName("Scale Line").Click();
+                break;
+            case "PopupViewer":
+                FindElementByName("PopupViewer").Click();
+                FindElementByName("Popup Viewer").Click();
+                break;
+            case null:
+            case "":
+                throw new ArgumentException("Sample name cannot be null or empty.", nameof(sampleName));
+            default:
+                throw new ArgumentException("Navigation for sample is not implemented. Consider modifying the Sample app to allow a more generic means to navigate to particular samples, then rewrite this method.", nameof(sampleName));
+        }
+    }
+}
