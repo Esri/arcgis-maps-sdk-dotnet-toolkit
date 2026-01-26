19b7d267e9fa02d2cfbf4b73100860ef5c468aac
imalcolm <imalcolm@esri.com>
2026-01-26 12:40:59 -0800

    Made WPF puppet app

---
+++ src/Tests/UITests/Toolkit.UITests.Wpf.Puppet/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Toolkit.UITests.Wpf.Puppet;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private string ApiKey = "API_KEY";
+
+    public App()
+    {
+        ControlPatcher.ApplyPatches();
+        Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.Initialize();
+        Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = ApiKey;
+    }
+}
---
+++ src/Tests/UITests/Toolkit.UITests.Wpf.Puppet/Tests/ScaleLineRenders.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using System.Windows.Controls;
+
+namespace Toolkit.UITests.Wpf.Puppet.Tests;
+
+/// <summary>
+/// Interaction logic for ScaleLineRender.xaml
+/// </summary>
+public partial class ScaleLineRenders : UserControl
+{
+    public ScaleLineRenders()
+    {
+        InitializeComponent();
+
+        var map = new Map(new Uri("https://www.arcgis.com/home/item.html?id=979c6cc89af9449cbeb5342a439c6a76"));
+        map.InitialViewpoint = new Viewpoint(new MapPoint(0, 0, SpatialReferences.WebMercator), 50000000);
+        MainMapView.Map = map;
+    }
+
+    private void UpdateViewpoint_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        var scale = double.Parse(ScaleTextBox.Text);
+        var latitude = double.Parse(LatitudeTextBox.Text);
+
+        var center = new MapPoint(0, latitude, SpatialReferences.Wgs84);
+        MainMapView.SetViewpoint(new Viewpoint(center, scale));
+    }
+}
