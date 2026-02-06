7037a1fa26b87a506ade4fd5ce2b81595f1b390e
imalcolm <imalcolm@esri.com>
2026-02-06 15:20:40 -0800

    Added mac/ios projects (untested) and made a mac solution filter

---
+++ src/Tests/UITests/Toolkit.UITests.MauiMac/AppiumSetup.MauiMac.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium.Appium;
+
+namespace Toolkit.UITest.Shared;
+
+public static partial class AppiumSetup
+{
+    [AssemblyInitialize]
+    public static void AssemblyInitialize(TestContext testContext)
+    {
+        var MauiSamplesApp = @"com.companyname.toolkit.sampleapp.maui";
+
+        driver = MakeMacDriver(MauiSamplesApp);
+
+        try
+        {
+            driver.Manage().Window.FullScreen();
+        }
+        catch
+        {
+            testContext.WriteLine("Could not fullscreen app. It may have already opened in fullscreen mode.");
+        }
+
+        var screenDensityElement = driver.FindElement(MobileBy.Id("ScreenDensity"));
+        ScreenDensity = float.Parse(screenDensityElement.GetAttribute("label"));
+    }
+}
---
+++ src/Tests/UITests/Toolkit.UITests.MauiiOS/AppiumSetup.MauiIOS.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Appium;
+
+namespace Toolkit.UITest.Shared;
+
+public static partial class AppiumSetup
+{
+    [AssemblyInitialize]
+    public static void AssemblyInitialize(TestContext testContext)
+    {
+        var MauiSamplesApp = @"com.companyname.toolkit.sampleapp.maui";
+        var deviceUdid = @"YOUR_DEVICE_UDID";
+
+        driver = MakeiOSDriver(deviceUdid, MauiSamplesApp);
+
+        var screenDensityElement = driver.FindElement(MobileBy.Id("ScreenDensity"));
+        ScreenDensity = float.Parse(screenDensityElement.GetAttribute("label"));
+    }
+}
