abf0a29c9383bd619eb4d6108886f390a2b093fc
Morten Nielsen <mn@iter.dk>
2026-05-18 17:10:42 -0700

    Add CircleStop icon

--- src/Toolkit/Toolkit/ToolkitIcons.cs
+++ src/Toolkit/Toolkit/ToolkitIcons.cs
@@ -54,4 +54,5 @@ internal static class ToolkitIcons
     public const string Inbox = "\uE8F4";
     public const string DownloadTo = "\uE81C";
     public const string Refresh = "\uEB66";
+    public const string CircleStop = "\uE7B3";
 }
