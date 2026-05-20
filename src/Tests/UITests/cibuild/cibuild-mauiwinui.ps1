64d1f524926c292cddae98a8a380b8be9fd647ed
imalcolm <imalcolm@esri.com>
2026-05-20 15:11:25 -0700

    Make the MauiWindows build use the latest MauiVersion

--- src/Tests/UITests/cibuild/cibuild-mauiwinui.ps1
+++ src/Tests/UITests/cibuild/cibuild-mauiwinui.ps1
@@ -9,15 +9,20 @@ if (-not $?) {
   exit 1
 }
 
-# Define build parameters
+# Define common build parameters
 $config_file = Join-Path $PSScriptRoot "variables.yml"
 $build_params = @('-c', 'Release', '-r', 'win-x64', '-p:UseMonoRuntime=false')
 if (![string]::IsNullOrWhiteSpace($env:RELEASE_VERSION)) {
   $build_params += "-p:UseNugetPackage=$env:RELEASE_VERSION"
 }
 
-$framework = "$(Get-YamlValue $config_file 'maui-dotnet-framework')-windows$(Get-YamlValue $config_file 'windows-sdk-version')"
-$build_params_app = @('-f', $framework)
+# Define app build parameters, including setting MauiVersion to the latest available
+$dotnet_version = Get-YamlValue $config_file 'dotnet-version'
+$dotnet_major_version = $(Select-String -InputObject $dotnet_version -Pattern '\d+\.\d+(?=\.)')[0].Matches[0].Value
+$build_params_app = @("-p:MauiVersion=${dotnet_major_version}*")
+
+$framework = "net${dotnet_major_version}-windows$(Get-YamlValue $config_file 'windows-sdk-version')"
+$build_params_app += @('-f', $framework)
 
 # Run tests
 $runner_name = 'Toolkit.UITests.MauiWinUI'
