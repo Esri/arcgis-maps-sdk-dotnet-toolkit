dee350cc1340556f7ab277a62dd85d79c23f7c94
imalcolm <imalcolm@esri.com>
2026-05-20 12:13:13 -0700

    Renamed UITEST_APP_PATH to TKUITEST_APP

--- src/Tests/UITests/Toolkit.UITests.WinUI/AppiumSetup.WinUI.cs
+++ src/Tests/UITests/Toolkit.UITests.WinUI/AppiumSetup.WinUI.cs
@@ -8,7 +8,7 @@ public static partial class AppiumSetup
     public static void AssemblyInitialize(TestContext testContext)
     {
         var WinUIAppPackageId = @"d733bdd1-d63f-45f9-b119-555748d3b3e4_6b5psgtf36ad0!App";
-        var envAppPath = Environment.GetEnvironmentVariable("UITEST_APP_PATH");
+        var envAppPath = Environment.GetEnvironmentVariable("TKUITEST_APP");
         var testApp = String.IsNullOrWhiteSpace(envAppPath) ? WinUIAppPackageId : envAppPath;
 
         driver = MakeWindowsDriver(testApp);
