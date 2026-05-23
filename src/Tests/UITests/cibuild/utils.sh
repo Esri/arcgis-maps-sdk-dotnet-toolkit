a494a48875d974623a5fdcb065306e212201d98a
ian13774 <ian13774@esri.com>
2026-05-22 18:18:22 -0700

    Node.js install

---
+++ src/Tests/UITests/cibuild/utils.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+function install_dotnet {
+  workspace=$1
+  dotnet_version=$2
+  dotnet_cache_dir=$3
+
+  # Install the desired dotnet version if not already cached
+  if [ -z "${dotnet_cache_dir}" ]; then
+    dotnet_install_dir="${workspace}/.dotnet"
+  else
+    dotnet_install_dir="${dotnet_cache_dir}/${dotnet_version}"
+  fi
+
+  if [ ! -x "${dotnet_install_dir}/dotnet" ]; then
+    mkdir -p "${dotnet_install_dir}"
+    curl -fsSL https://dot.net/v1/dotnet-install.sh -o "${workspace}/dotnet-install.sh"
+    bash "${workspace}/dotnet-install.sh" --version "${dotnet_version}" --install-dir "${dotnet_install_dir}"
+  fi
+
+  export DOTNET_EXE="${dotnet_install_dir}"/dotnet
+}
+
+function read_yaml_var {
+  yml_file=$1
+  varname=$2
+  grep "^${varname}" "${yml_file}" | sed -E "s/^${varname}: \"(.*)\"/\1/"
+}
