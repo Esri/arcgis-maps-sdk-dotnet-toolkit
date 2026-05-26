2ed1dcfd93560dc3c246e360f4185f5910cf958b
ian13774 <ian13774@esri.com>
2026-05-26 10:05:31 -0700

    Driver install, nuget repo, app builds, exceptions in RunBinary

--- src/Tests/UITests/cibuild/utils.cs
+++ src/Tests/UITests/cibuild/utils.cs
@@ -6,6 +6,7 @@ internal class Program
 {
     static int Main(string[] args)
     {
+        // Required inputs
         var workspace = Environment.GetEnvironmentVariable("WORKSPACE");
         var dotnetExe = Environment.GetEnvironmentVariable("DOTNET_EXE");
         var yamlConfig = Environment.GetEnvironmentVariable("YAML_CONFIG");
@@ -17,15 +18,81 @@ internal class Program
             throw new ArgumentException("Workspace and dotnet directory must be existing paths.");
         }
 
+        // Derived variables
+        var toolkitSrc = Path.GetFullPath(Path.Join(Path.GetDirectoryName(yamlConfig), "..", "..", ".."));
+
+        // Install maui
+        RunBinary(dotnetExe, "workload install maui-android");
+
+        // Install node
         var nodeWorkspace = Path.Join(workspace, ".node");
         var nodeVersion = ReadYamlValue(yamlConfig, "node-version");
         var (nodeExe, npmExe) = InstallNode(nodeWorkspace, nodeVersion);
 
+        // Install appium
+        Environment.SetEnvironmentVariable("APPIUM_HOME", Path.Join(workspace, ".appium"));
+        var appiumEntry = Path.Join(nodeWorkspace, "node_modules", "appium", "index.js");
         RunBinary(npmExe, $"install appium --prefix \"{nodeWorkspace}\"");
 
-        // TODO: Install android driver
+        // Install appium android driver
+        try {
+            RunBinary(nodeExe, $"\"{appiumEntry}\" driver install uiautomator2");
+
+            // Additional setup for android? May need to have build task set ANDROID_HOME, JAVA_HOME,
+            // and add adb to path for session if not already there. bundletool.jar probably not
+            // required, can just run the app on device using dotnet anyway
+        }
+        catch {
+            Console.WriteLine("Appium driver install failed. This may be a real error, or the driver may already be installed. Check preceeding logs for details.");
+        }
+
+        var nugetRepo = Environment.GetEnvironmentVariable("NUGET_REPO");
+        if (!String.IsNullOrWhiteSpace(nugetRepo)) {
+            SetNugetSource(workspace, dotnetExe, nugetRepo);
+        }
+
+        var buildParamsCommon = new List<string>() {
+            "-c Release",
+            "-p:ArtifactsPivots=TestBuild",
+            "-p:UseArtifactsOutput=true"
+        };
+
+        var androidFramework = "net10.0-android";
+        var buildParamsApp = new List<string>() {
+            $"-f {androidFramework}",
+            $"-p:TargetFrameworks={androidFramework}",
+            "-r android-arm64"
+        };
+        var releaseVersion = Environment.GetEnvironmentVariable("RELEASE_VERSION");
+        if (!String.IsNullOrWhiteSpace(releaseVersion)) {
+            buildParamsApp.Add($"-p:UseNugetPackage={releaseVersion}");
+        }
+
+        var appPath = Path.Join(toolkitSrc, "Tests", "UITests", "Toolkit.UITests.Maui.App", "Toolkit.UITests.Maui.App.csproj");
+        RunBinary(dotnetExe, $"build {appPath} {String.Join(" ", buildParamsCommon)} {String.Join(" ", buildParamsApp)}");
+
+        var runnerPath = Path.Join(toolkitSrc, "Tests", "UITests", "Toolkit.UITests.MauiAndroid", "Toolkit.UITests.MauiAndroid.csproj");
+        RunBinary(dotnetExe, $"build {runnerPath} {String.Join(" ", buildParamsCommon)}");
+
+
         
         return 0;
+    }
+
+    private static void SetNugetSource(string workspace, string dotnetExe, string nugetRepo)
+    {
+        Console.WriteLine("Configuring nuget...");
+
+        RunBinary(dotnetExe, $"new nugetconfig --force -o \"{workspace}\"");
+
+        var configFile = Path.Join(workspace, "nuget.config");
+        RunBinary(dotnetExe, $"nuget add source \"{nugetRepo}\" --configfile \"{configFile}\"");
+
+        var nugetDir = Path.Join(workspace, ".nuget");
+        Environment.SetEnvironmentVariable("NUGET_PACKAGES", Path.Join(nugetDir, "packages"));
+        Environment.SetEnvironmentVariable("NUGET_HTTP_CACHE_PATH", Path.Join(nugetDir, "cache"));
+
+        Console.WriteLine("Done configuring nuget.\n");
     }
 
     private static string ReadYamlValue(string yamlFile, string name) {
@@ -96,6 +163,7 @@ internal class Program
             FileName = binary,
             Arguments = arguments,
             RedirectStandardOutput = true,
+            RedirectStandardError = true,
             UseShellExecute = false
         };
 
@@ -106,9 +174,20 @@ internal class Program
                 Console.WriteLine(e.Data);
             }
         };
+        process.ErrorDataReceived += (sender, e) => {
+            if (!string.IsNullOrEmpty(e.Data)) {
+                Console.Error.WriteLine(e.Data);
+            }
+        };
 
         process.Start();
         process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
         process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            throw new Exception($"Command failed with exit code {process.ExitCode}: {binary} {arguments}");
+        }
     }
 }
