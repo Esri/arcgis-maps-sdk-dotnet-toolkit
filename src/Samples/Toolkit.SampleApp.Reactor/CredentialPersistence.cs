6f8ed2eb5647f8f3e49f6784b3423c50f258d32e
dotMorten <mn@iter.dk>
2026-05-30 10:38:42 -0700

    Add authentication support

---
+++ src/Samples/Toolkit.SampleApp.Reactor/CredentialPersistence.cs
@@ -0,0 +1,105 @@
+﻿using Esri.ArcGISRuntime;
+using Esri.ArcGISRuntime.Security;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Services.Maps.LocalSearch;
+
+namespace Toolkit.SampleApp.Reactor
+{
+    internal class SecureStorageCredentialPersistence : CredentialPersistence, ILoadable
+    {
+        ConcurrentBag<Credential> _credentials = new ConcurrentBag<Credential>();
+
+        public event EventHandler<EventArgs>? Loaded;
+        public event EventHandler<LoadStatusEventArgs>? LoadStatusChanged;
+
+        public Exception? LoadError { get; private set; }
+
+        public LoadStatus LoadStatus { get; private set; }
+
+        public SecureStorageCredentialPersistence()
+        {
+        }
+
+        private Task? _loadTask;
+
+        public Task LoadAsync()
+        {
+            return _loadTask ??= LoadAsync_Impl();
+        }
+        private async Task LoadAsync_Impl()
+        {
+            LoadStatus = LoadStatus.Loading;
+            try
+            {
+                await LoadCredentialsAsync();
+                LoadStatus = LoadStatus.Loaded;
+                Loaded?.Invoke(this, EventArgs.Empty);
+            }
+            catch(System.Exception ex)
+            {
+                LoadStatus = LoadStatus.FailedToLoad;
+                LoadError = ex;
+            }
+        }
+        protected override void Add(Credential credential)
+        {
+            _credentials.Add(credential);
+            SaveCredentials();
+        }
+        protected override void Remove(Credential credential)
+        {
+            _credentials.TryTake(out _);
+            SaveCredentials();
+        }
+        protected override IEnumerable<Credential> GetCredentials()
+        {
+            return _credentials;
+        }
+        protected override void Update(Credential credential)
+        {
+            SaveCredentials();
+        }
+        protected override void Clear()
+        {
+            _credentials.Clear();
+            SaveCredentials();
+        }
+        private async Task LoadCredentialsAsync()
+        {
+            _credentials.Clear();
+            string? json = await SecureStorage.GetAsync("credentials");
+            if (json is null) return;
+            foreach (var cred in json.Split('\n'))
+            {
+                try
+                {
+                    _credentials.Add(Credential.FromJson(cred));
+                }
+                catch { }
+            }
+        }
+
+        private void SaveCredentials()
+        {
+            // TODO: Make thread safe and avoid saving if there are multiple updates in a short time
+            var json = string.Join("\n", _credentials.Select(c => c.ToJson()));
+            _ = SecureStorage.SetAsync("credentials", json);
+        }
+
+        public void CancelLoad()
+        {
+        }
+
+
+        public Task RetryLoadAsync()
+        {
+            LoadError = null;
+            _loadTask = null;
+            return LoadAsync();
+        }
+    }
+}
--- src/Samples/Toolkit.SampleApp.Reactor/Program.cs
+++ src/Samples/Toolkit.SampleApp.Reactor/Program.cs
@@ -3,17 +3,49 @@ using Microsoft.UI.Reactor.Core;
 using Microsoft.UI.Reactor.Hosting;
 using static Microsoft.UI.Reactor.Factories;
 using Toolkit.SampleApp.Reactor;
+using Esri.ArcGISRuntime.Security;
 
-ReactorApp.Run<GalleryShell>("WinUI Gallery (Reactor)", width: 1400, height: 900,
+if (WinUIEx.WebAuthenticator.CheckOAuthRedirectionActivation())
+    return;
+
+ReactorApp.Run<GalleryShell>("WinUI Gallery (Reactor)", width: 1200, height: 800,
     configure: host =>
     {
-        Init();
+        InitMapsSDK();
         XamlInterop.Register(host.Reconciler);
         Esri.ArcGISRuntime.Toolkit.Reactor.Interop.Register(host.Reconciler);
     });
 
-async void Init()
+async void InitMapsSDK()
 {
     if (await SecureStorage.GetAsync("APIKey") is string key)
         Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = key;
+    else
+        AuthenticationManager.Current.Persistence = CredentialPersistence.CreateDefault() ?? new SecureStorageCredentialPersistence();
+
+    AuthenticationManager.Current.OAuthUserConfigurations.Add(
+        new OAuthUserConfiguration(
+            new Uri("https://www.arcgis.com/portal"),
+            clientId: "rdpQIZlaemFowcdn",
+            redirectUrl: new Uri("agsreactor://")
+           ));
+    AuthenticationManager.Current.OAuthHandler = new OAuthHandler();
 }
+
+internal class OAuthHandler : IOAuthHandler
+{
+    public async Task<IDictionary<string, string>> LoginAsync(OAuthLoginParameters parameters)
+    {
+        Microsoft.Windows.AppLifecycle.ActivationRegistrationManager.RegisterForProtocolActivation("agsreactor", "Assets\\Square150x150Logo.scale-100", "ArcGIS Reactor Sample", null);
+        try
+        {
+            var result = await WinUIEx.WebAuthenticator.AuthenticateAsync(parameters.AuthorizeUri, parameters.RedirectUri);
+            return result.Properties;
+        }
+        finally
+        {
+            Microsoft.Windows.AppLifecycle.ActivationRegistrationManager.UnregisterForProtocolActivation("agsreactor", null);
+        }
+    }
+}
+
