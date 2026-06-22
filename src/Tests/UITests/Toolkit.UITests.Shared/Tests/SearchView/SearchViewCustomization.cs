1ac8e68521e17325597bafd89ce86fee7e495db0
Samhitha Burugupalli <sburugupalli@esri.com>
2026-06-22 10:26:58 -0700

    Update src/Tests/UITests/Toolkit.UITests.Shared/Tests/SearchView/SearchViewCustomization.cs
    Co-authored-by: Ian Malcolm <imalcolm@esri.com>

--- src/Tests/UITests/Toolkit.UITests.Shared/Tests/SearchView/SearchViewCustomization.cs
+++ src/Tests/UITests/Toolkit.UITests.Shared/Tests/SearchView/SearchViewCustomization.cs
@@ -96,7 +96,7 @@ public class SearchViewCustomization : AppiumTestBase
 #if !MAUI_TEST
         var allSourcesButton = FindElement("AllSourcesButton", TimeSpan.FromSeconds(5));
         Assert.AreEqual(expected.AllSourcesButtonText, allSourcesButton.Text, $"Expected the all sources button text to be set to {expected.Type} value.");
-        Assert.AreEqual(expected.AllSourcesButtonText, GetAutomationName(allSourcesButton), $"Expected the automation name of all sources button to be set to {expected.Type} value.");
+        Assert.AreEqual(expected.AllSourcesButtonText, GetAutomationName(allSourcesButton), $"Expected the automation name of the all sources button to be set to {expected.Type} value.");
 #else
         Assert.IsTrue(ElementExistsByText(expected.AllSourcesButtonText, TimeSpan.FromSeconds(5)), $"Expected the all sources button text to be set to {expected.Type} value.");
 #endif
