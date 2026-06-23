a39e6b887b8b6ffdc550087fa5bbac0d3fb8931c
imalcolm <imalcolm@esri.com>
2026-06-23 10:07:46 -0700

    Hacky partial implementation

---
+++ src/Toolkit/Toolkit/UI/Controls/OrientedImagery/OrientedImageryView.Windows.cs
@@ -0,0 +1,10 @@
+#if WPF // Limiting this to WPF for now to keep things simple
+
+namespace Esri.ArcGISRuntime.Toolkit.UI.Controls;
+
+[TemplatePart(Name = ImageDisplayName, Type = typeof(OrientedImageDisplay))]
+public partial class OrientedImageryView : Control
+{
+}
+
+#endif
