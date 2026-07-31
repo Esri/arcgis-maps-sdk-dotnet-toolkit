46be509f4722a587aef28d349c2b429f52a49a1a
Ian Malcolm <imalcolm@esri.com>
2026-07-31 12:20:44 -0700

    UITests: Fix windows ci runs by placing koffi in the appium folder (#816)

--- src/Tests/UITests/cibuild/utils.psm1
+++ src/Tests/UITests/cibuild/utils.psm1
@@ -71,7 +71,7 @@ function Invoke-WindowsUITests {
   }
 
   # Install koffi
-  & $npm_exe install koffi --prefix $node_workspace
+  & $npm_exe install koffi --prefix $env:APPIUM_HOME
   if ($LASTEXITCODE -ne 0) {
     Write-Error "Error installing koffi node module. Koffi is necessary to avoid reliance on node-ffi, which in turn requires a Visual Studio install."
     exit $LASTEXITCODE
